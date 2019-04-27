90351cbad18382391d089e5104d28d79d377f7d2
ferisef <ferisefbns@gmail.com>
2019-04-27 15:27:50 +0300

    Changed Accept header

--- src/middlewares/validate-headers/index.js
+++ src/middlewares/validate-headers/index.js
@@ -2,7 +2,7 @@ const API_VERSION = 1;
 
 export function validateAccept() {
   return (req, res, next) => {
-    if (!req.headers || req.headers.accept !== `application/vnd.moosik.${API_VERSION}+json`) {
+    if (!req.headers || req.headers.accept !== `application/vnd.moosik.v${API_VERSION}+json`) {
       res.status(405).send({ message: 'Incorrect `Accept` header provided.' });
       return;
     }
