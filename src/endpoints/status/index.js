536c189b5252a6c8b2afa92bcb2eadc0d0626c03
ferisef <ferisefbns@gmail.com>
2019-06-09 15:40:35 +0300

    Update func name

--- src/endpoints/status/index.js
+++ src/endpoints/status/index.js
@@ -1,12 +1,11 @@
-import { getDiskInfo } from '../../apis/yandex-disk';
-
+import { getDiskStatus } from '../../apis/yandex-disk';
 
 export default function () {
   return async (req, res) => {
     try {
-      res.status(200).send(await getDiskInfo());
+      res.status(200).send(await getDiskStatus());
     } catch (e) {
-      res.status(500).send();
+      res.status(500).send({ message: 'Internal server error.' });
     }
   };
 }
