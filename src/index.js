6ede57a81310830d5a985a64c0ce19d272308b9c
ferisef <ferisefbns@gmail.com>
2019-06-09 15:42:05 +0300

    Add error handling

--- src/index.js
+++ src/index.js
@@ -1,52 +1,36 @@
 import Path from 'path';
 import Express from 'express';
-import Cors from 'cors';
+import cors from 'cors';
 import BodyParser from 'body-parser';
+import errorHandler from './middlewares/errorHandler';
+import mongoDB from './apis/mongodb';
+import yandexDiskApi from './apis/yandex-disk';
+import connectApi from './api';
 
-import YandexDiskApi from './apis/yandex-disk';
-import MongoDB from './apis/mongodb';
+const { PORT, YANDEX_DISK_API_TOKEN } = process.env;
 
-import Api from './api';
-
-
-YandexDiskApi(process.env.YANDEX_DISK_API_TOKEN);
-MongoDB();
-
+mongoDB();
+yandexDiskApi(YANDEX_DISK_API_TOKEN);
 
 const app = Express();
-
 
 app.use(Express.static(Path.resolve('./static')));
 
 app.use(BodyParser.json());
 app.use(BodyParser.raw({ type: 'audio/mpeg', limit: '10mb' }));
 
-app.use(Cors());
+app.use(cors());
 
-app.use((error, req, res, next) => {
-  if (!error) next();
-
-  switch (error.message) {
-    case 'request entity too large':
-      res.send({ message: 'File too large.' });
-      break;
-    default:
-      res.status(500).send();
-  }
-});
-
+app.use(errorHandler());
 
 app.get('/', (req, res) => {
   res.sendFile(Path.resolve('./html/index.html'));
 });
 
-
-Api(app);
-
+connectApi(app);
 
 app.get('*', (req, res) => {
   res.redirect('/');
 });
 
-
-app.listen(process.env.PORT || 8080);
+app.listen(PORT || 8080);
