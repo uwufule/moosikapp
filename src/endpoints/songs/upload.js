8ee0362fc09ba6d1b24a516753804f8be4cc5e90
ferisef <ferisefbns@gmail.com>
2019-06-09 15:38:29 +0300

    Add DiskResourceAlreadyExistsError error

--- src/endpoints/songs/upload.js
+++ src/endpoints/songs/upload.js
@@ -1,21 +1,14 @@
 import FS from 'fs';
 import Crypto from 'crypto';
-import Uuid from 'uuid';
+import uuidv4 from 'uuid/v4';
 import { uploadFileFromStream } from '../../apis/yandex-disk';
 import { saveSong } from '../../apis/mongodb/songs';
 
-
 const TEMP_FILE = 'tmp';
 
-
 export default function (req, res) {
-  if (req.headers['content-type'] !== 'audio/mpeg') {
-    res.send({ message: 'No data provided.' });
-    return;
-  }
-
   const filename = req.headers['x-uploaded-filename'] || '';
-  const [author, title] = filename.replace('.mp3', '').split('-').map(a => a.trim());
+  const [author, title] = filename.split('-').map(s => s.trim());
 
   const writeStream = FS.createWriteStream(TEMP_FILE, { flags: 'w+', encoding: 'binary' });
 
@@ -23,7 +16,7 @@ export default function (req, res) {
     const hash = Crypto.createHash('sha256').update(FS.readFileSync(TEMP_FILE)).digest('hex');
     const remotePath = `/${hash}.mp3`;
 
-    const uuid = Uuid();
+    const uuid = uuidv4();
 
     const readStream = FS.createReadStream(TEMP_FILE, { autoClose: true });
 
@@ -36,7 +29,13 @@ export default function (req, res) {
 
       res.status(201).send({ message: 'You have successfully uploaded a new song.', uuid });
     } catch (e) {
-      res.status(406).send({ message: 'Error while uploading.' });
+      switch (e.error) {
+        case 'DiskResourceAlreadyExistsError':
+          res.status(406).send({ message: 'Already exists.' });
+          break;
+        default:
+          res.status(406).send({ message: 'Error while uploading.' });
+      }
     } finally {
       readStream.resume();
       FS.unlinkSync(TEMP_FILE);
