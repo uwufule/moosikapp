993ae0c4d40fdef5fe2f5474616595e22e63a2f9
ferisef <ferisefbns@gmail.com>
2019-06-14 22:04:45 +0300

    Change status response

--- src/endpoints/status/index.js
+++ src/endpoints/status/index.js
@@ -3,7 +3,8 @@ import { getDiskStatus } from '../../apis/yandex-disk';
 export default function () {
   return async (req, res) => {
     try {
-      res.status(200).send(await getDiskStatus());
+      const storage = await getDiskStatus();
+      res.status(200).send({ message: 'Successful retrieving status.', storage });
     } catch (e) {
       res.status(500).send({ message: 'Internal server error.' });
     }
