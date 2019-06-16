80e1c0e99818e6c87642fcb8572177df4b4b1836
ferisef <ferisefbns@gmail.com>
2019-06-16 18:40:56 +0300

    First build

---
+++ static/precache-manifest.aae6b21d510226989017b2a711c79cc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a930ad9ca8551afa7454585ef615ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b47b0f826dfbe35b3372",
+    "url": "/static/css/main.afec7306.chunk.css"
+  },
+  {
+    "revision": "60a121dbae1bb0e2bdb0",
+    "url": "/static/js/2.1bc20fc4.chunk.js"
+  },
+  {
+    "revision": "b47b0f826dfbe35b3372",
+    "url": "/static/js/main.a7bbacd6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35f444d6be8795d6047663d7c477c520",
+    "url": "/static/media/bg.35f444d6.jpg"
+  },
+  {
+    "revision": "1ac1792428da134e6c13cd2271ee3f89",
+    "url": "/static/media/icons.1ac17924.svg"
+  },
+  {
+    "revision": "229871f728961b3478ebc6af96a4f63c",
+    "url": "/static/media/icons.229871f7.svg"
+  }
+]);
