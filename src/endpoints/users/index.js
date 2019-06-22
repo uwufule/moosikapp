84a9d495a15f79776ef254222a463af474fa474d
ferisef <ferisefbns@gmail.com>
2019-06-22 16:40:13 +0300

    Update getUser. Add decodeURI

--- src/endpoints/users/index.js
+++ src/endpoints/users/index.js
@@ -1,16 +1,19 @@
 import { getUser } from '../../apis/mongodb/users';
 
-
 export default function () {
   return async (req, res) => {
+    const { username } = req.params;
+
     try {
-      const user = await getUser(req.params.username);
+      const user = await getUser(decodeURI(username));
+
       if (!user) {
         res.status(404).send({ message: 'No user found.' });
         return;
       }
+
       res.status(200).send({ message: 'Successfully retrieved user.', user });
-    } catch (error) {
+    } catch (e) {
       res.status(500).send({ message: 'Internal server error.' });
     }
   };
