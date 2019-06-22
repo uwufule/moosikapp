518f6c69d8baaa1f45e3b77f7d0fd4d46f4c2846
ferisef <ferisefbns@gmail.com>
2019-06-22 16:34:00 +0300

    Add getLikedSongs

--- src/apis/mongodb/songs.js
+++ src/apis/mongodb/songs.js
@@ -2,24 +2,26 @@ import SongModel from './models/song';
 
 export async function getSongs(skip = 0, limit = 100) {
   const projection = {
-    uuid: 1, author: 1, title: 1, cover: 1, _id: 0,
+    _id: 0,
+    uuid: 1,
+    author: 1,
+    title: 1,
+    cover: 1,
   };
 
-  const songs = await SongModel.find({ }, projection).skip(skip).limit(limit);
+  const songs = await SongModel.find({}, projection).skip(skip).limit(limit);
   return songs;
 }
 
 export async function getSongByUuid(uuid) {
   const projection = {
+    _id: 0,
     author: 1,
     title: 1,
-    uploadedBy: 1,
     cover: 1,
-    hash: 1,
-    type: 1,
     url: 1,
+    uploadedBy: 1,
     createdAt: 1,
-    _id: 0,
   };
 
   const song = await SongModel.findOne({ uuid }, projection);
@@ -31,23 +33,46 @@ export async function findSongs(queryString) {
     $or: [
       {
         author: {
-          $regex: queryString, $options: 'i',
+          $regex: queryString,
+          $options: 'i',
         },
       },
       {
         title: {
-          $regex: queryString, $options: 'i',
+          $regex: queryString,
+          $options: 'i',
         },
       },
     ],
   };
 
   const projection = {
-    uuid: 1, author: 1, title: 1, cover: 1, _id: 0,
+    _id: 0,
+    uuid: 1,
+    author: 1,
+    title: 1,
+    cover: 1,
   };
 
   const foundSongs = await SongModel.find(query, projection);
   return foundSongs;
+}
+
+export async function getLikedSongs(userUuid) {
+  const query = {
+    likes: userUuid,
+  };
+
+  const projection = {
+    _id: 0,
+    uuid: 1,
+    author: 1,
+    title: 1,
+    cover: 1,
+  };
+
+  const likedSongs = await SongModel.find(query, projection);
+  return likedSongs;
 }
 
 export async function saveSong(songData) {
