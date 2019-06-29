ac9b5051221cf55cfbc6a8f3b07abc8f073068c6
ferisef <ferisefbns@gmail.com>
2019-06-29 14:30:38 +0300

    Change accept header (vnd.moosikapp.v*)

--- src/middlewares/headers/index.js
+++ src/middlewares/headers/index.js
@@ -2,7 +2,7 @@ const { API_VERSION } = process.env;
 
 export function validateAccept() {
   return (req, res, next) => {
-    if (!req.headers || req.headers.accept !== `application/vnd.moosik.v${API_VERSION}+json`) {
+    if (!req.headers || req.headers.accept !== `application/vnd.moosikapp.v${API_VERSION}+json`) {
       res.status(405).send({ message: 'Incorrect `Accept` header provided.' });
       return;
     }
