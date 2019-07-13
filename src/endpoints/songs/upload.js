21f3e3460e73bddba2a8d21fdbe951233e5f9304
ferisef <ferisefbns@gmail.com>
2019-07-13 16:20:52 +0300

    Update for working with cdn

--- src/endpoints/songs/upload.js
+++ src/endpoints/songs/upload.js
@@ -1,42 +1,70 @@
-import FS from 'fs';
-import Crypto from 'crypto';
+import { Buffer } from 'buffer';
+import { Readable } from 'stream';
 import uuidv4 from 'uuid/v4';
-import { uploadFileFromStream } from '../../apis/yandex-disk';
+import request from 'request';
 import { saveSong } from '../../apis/mongodb/songs';
 
-const TEMP_FILE = 'tmp';
+const { CDN_SERVER } = process.env;
+
+const uploadTargetList = {};
 
 export default function (req, res) {
-  const writeStream = FS.createWriteStream(TEMP_FILE, { flags: 'w+', encoding: 'binary' });
+  const uploadTarget = uuidv4();
 
-  writeStream.end(req.body, async () => {
-    const hash = Crypto.createHash('sha256').update(FS.readFileSync(TEMP_FILE)).digest('hex');
+  uploadTargetList[uploadTarget] = setTimeout(() => {
+    delete uploadTargetList[uploadTarget];
+  }, 600000);
 
-    const path = `/${hash}.mp3`;
+  const { ext = 'mp3' } = req.query;
+  const dext = Buffer.from(ext, 'utf8').toString('hex');
 
-    const uuid = uuidv4();
+  const readable = new Readable();
+  readable._read = () => {}; // eslint-disable-line no-underscore-dangle
+  readable.push(req.body);
+  readable.push(null);
 
-    const readStream = FS.createReadStream(TEMP_FILE, { autoClose: true });
+  const targetUri = `${CDN_SERVER}/upload-target/${uploadTarget}.${dext}`;
 
-    try {
-      await uploadFileFromStream(path, readStream);
+  readable.pipe(request.put(targetUri, async (error, { statusCode }, body) => {
+    if (error) {
+      res.status(500).send({ message: 'Internal server error.' });
+      return;
+    }
+
+    if (statusCode === 201) {
+      const uuid = uuidv4();
 
       await saveSong({
-        uuid, uploadedBy: req.jwt.uuid, path, likes: [req.jwt.uuid],
+        uuid,
+        uploadedBy: req.jwt.uuid,
+        path: body,
+        likes: [req.jwt.uuid],
       });
 
-      res.status(201).send({ message: 'You have successfully uploaded a new song.', uuid });
-    } catch (e) {
-      switch (e.message) {
-        case 'DiskResourceAlreadyExistsError':
-          res.status(406).send({ message: 'Already exists.' });
-          break;
-        default:
-          res.status(406).send({ message: 'Error while uploading.' });
-      }
-    } finally {
-      readStream.resume();
-      FS.unlinkSync(TEMP_FILE);
+      res.status(statusCode).send({
+        message: 'You have successfully uploaded a new song.',
+        uuid,
+      });
+      return;
     }
-  });
+
+    const message = body || 'Error while uploading.';
+    res.status(400).send({ message });
+  }));
+}
+
+export function verify() {
+  return (req, res) => {
+    const { uuid } = req.query;
+
+    if (!uploadTargetList[uuid]) {
+      res.status(400).send();
+      return;
+    }
+
+    res.status(200).send();
+
+    delete uploadTargetList[uuid];
+    clearTimeout(uploadTargetList[uuid]);
+  };
 }
