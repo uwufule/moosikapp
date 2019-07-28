8a7dcae812f0fdaf3412029c5777c398c88e579b
ferisef <ferisefbns@gmail.com>
2019-07-28 21:15:31 +0300

    Test build

---
+++ static/precache-manifest.f8a3f4880eadee9e36aa78ced2e0a754.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a175261b1c0c5d9a58d6f6cb38f6a9",
+    "url": "/ferisef/moosikapp-web/index.html"
+  },
+  {
+    "revision": "56db98f66c39823092be",
+    "url": "/ferisef/moosikapp-web/static/css/main.2f8a78c4.chunk.css"
+  },
+  {
+    "revision": "b8a55275e416ede5b7e4",
+    "url": "/ferisef/moosikapp-web/static/js/2.eb5bbc25.chunk.js"
+  },
+  {
+    "revision": "56db98f66c39823092be",
+    "url": "/ferisef/moosikapp-web/static/js/main.11a0083c.chunk.js"
+  },
+  {
+    "revision": "c16d0fb8fd1188c8d2ea",
+    "url": "/ferisef/moosikapp-web/static/js/runtime~main.8135ad0b.js"
+  },
+  {
+    "revision": "174b8195b113b95a3795c5a9d87c624b",
+    "url": "/ferisef/moosikapp-web/static/media/IconPack.174b8195.svg"
+  },
+  {
+    "revision": "6b9d14e4367365d421842f65379af143",
+    "url": "/ferisef/moosikapp-web/static/media/IconPack.6b9d14e4.svg"
+  },
+  {
+    "revision": "003204dcf741b994f82e1ee1f8e14f7b",
+    "url": "/ferisef/moosikapp-web/static/media/bg.003204dc.jpg"
+  },
+  {
+    "revision": "d36aebff369e10b8b6fd93347aa3a1ca",
+    "url": "/ferisef/moosikapp-web/static/media/bg.d36aebff.webp"
+  }
+]);
--- static/service-worker.js
+++ static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a33b7062b58eb94bdfb0da51506b18d0.js"
+  "/ferisef/moosikapp-web/precache-manifest.f8a3f4880eadee9e36aa78ced2e0a754.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ferisef/moosikapp-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
