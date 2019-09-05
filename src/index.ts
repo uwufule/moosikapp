2952f1937f00ac04f946230e4a0b394af73eba77
Ferisef <ferisefbns@gmail.com>
2019-09-05 21:33:59 +0300

    Update json import

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import Path from 'path';
-import Express, { Application } from 'express';
+import Express from 'express';
 import cors from 'cors';
 import BodyParser from 'body-parser';
 import errorHandler from './middlewares/errorHandler';
 import mongoDB from './apis/mongodb';
 import withApi from './endpoints';
 
-const { MAX_FILE_SIZE } = require('./config.json');
+import { MAX_FILE_SIZE } from './config.json';
 
 const { PORT } = process.env;
 
 mongoDB();
 
-const app: Application = Express();
+const app = Express();
 
 app.disable('x-powered-by');
 app.disable('etag');
