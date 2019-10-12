b7af576af356566f71926d70e3dcf59e24276295
ferisef <ferisefbns@gmail.com>
2019-10-13 00:07:35 +0300

    Update

---
+++ static/precache-manifest.2a24dafdfbf44b00b115ae778f38f3e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd64bf2debdae68c7783cb64e82ae70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe042aecbe6e1e3fa51",
+    "url": "/static/css/main.31191633.chunk.css"
+  },
+  {
+    "revision": "af4fb7fb26e6f0cdc36c",
+    "url": "/static/js/2.f1c08ddc.chunk.js"
+  },
+  {
+    "revision": "efe042aecbe6e1e3fa51",
+    "url": "/static/js/main.760b09ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "003204dcf741b994f82e1ee1f8e14f7b",
+    "url": "/static/media/bg.003204dc.jpg"
+  },
+  {
+    "revision": "d36aebff369e10b8b6fd93347aa3a1ca",
+    "url": "/static/media/bg.d36aebff.webp"
+  }
+]);
