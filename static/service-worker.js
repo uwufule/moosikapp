58827f9c15b2c5ab333d001b60f96b45f17c8574
ferisef <ferisefbns@gmail.com>
2019-10-13 00:04:24 +0300

    Update

--- static/service-worker.js
+++ static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99f779369fb73b9500abd81a000d769a.js"
+  "/precache-manifest.2a24dafdfbf44b00b115ae778f38f3e5.js"
 );
 
 self.addEventListener('message', (event) => {
