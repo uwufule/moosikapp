66ea41b2e05fb75c446d715a22753c1523135c89
Ferisef <ferisefbns@gmail.com>
2019-10-15 13:49:23 +0300

    Add return to if (!req.body). Update import

--- src/endpoints/v2/register/index.ts
+++ src/endpoints/v2/register/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import Crypto from 'crypto';
 import uuidv4 from 'uuid/v4';
-import * as DB from '../../../apis/mongodb/users';
+import { createUser } from '../../../apis/mongodb/users';
 
 const EMAIL_REGEX = /^\w+[\w-.]*@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/;
 
@@ -9,9 +9,11 @@ export default () => {
   return async (req: Request, res: Response): Promise<void> => {
     if (!req.body) {
       res.status(400).send({ message: 'No body provided.' });
+      return;
     }
 
     const { email, username, password } = req.body;
+
     if (!email || !EMAIL_REGEX.test(email)) {
       res.status(400).send({ message: 'Invalid e-mail address provided.' });
       return;
@@ -33,16 +35,19 @@ export default () => {
     const uuid = uuidv4();
 
     try {
-      await DB.createUser({
+      await createUser({
         uuid, username, email, password: { hash: `${salt}.${hash}` },
       });
 
       res.status(201).send({ message: 'You have successfully created a new account.', uuid });
     } catch (e) {
       if (e.errmsg.startsWith('E11000')) {
-        res.status(400).send({ message: 'An account with that email address and/or username already exists.' });
+        res.status(400).send({
+          message: 'An account with that email address and/or username already exists.',
+        });
         return;
       }
+
       res.status(500).send({ message: 'Internal server error.' });
     }
   };
