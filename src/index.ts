528184faa97d2f8d7083c850e1313407e4fd4655
Ferisef <ferisefbns@gmail.com>
2019-11-20 20:45:31 +0300

    Add default port value

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,15 @@ import withApi from './endpoints';
 
 import { MAX_FILE_SIZE } from './config.json';
 
-const { PORT } = process.env;
+const { PORT = 8080 } = process.env;
 
 mongoDB();
 
 const app = Express();
 
-app.use(helmet({
-  hsts: false,
-}))
+app.use(helmet({ hsts: false }));
 
-app.use(Express.static(Path.resolve('./static')));
+app.use(Express.static(Path.resolve('static')));
 
 app.use(BodyParser.json());
 app.use(BodyParser.raw({ type: 'audio/mpeg', limit: MAX_FILE_SIZE }));
