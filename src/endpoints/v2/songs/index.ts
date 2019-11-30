4b6dd7e35062098e70f76e056a4daa5983ffdf61
Ferisef <ferisefbns@gmail.com>
2019-11-30 20:32:19 +0300

    Use query params parser. Fix

--- src/endpoints/v2/songs/index.ts
+++ src/endpoints/v2/songs/index.ts
@@ -1,8 +1,9 @@
 import { Response, NextFunction } from 'express';
 import { AuthorizedRequest } from '../../../middlewares/authorization';
 import upload from './upload';
-import * as DB from '../../../apis/mongodb/songs';
-import { getUserByUuid } from '../../../apis/mongodb/users';
+import * as Songs from '../../../api/mongodb/songs';
+import * as Users from '../../../api/mongodb/users';
+import parseQueryParams from '../../../utils/queryParams';
 import APIError from '../../../errors/APIError';
 
 import { scopes, roles } from '../../../config.json';
@@ -20,35 +21,17 @@ interface SongData {
 
 export function getSongs() {
   return async (req: AuthorizedRequest, res: Response, next: NextFunction) => {
-    const { scope } = req.query;
-
-    const skip = Number(req.query.skip);
-    if (skip < 0) {
-      next(new APIError(400, 'Invalid query parameter `skip` provided.'));
-      return;
-    }
-
-    const limit = Number(req.query.limit);
-    if (limit < 1 || limit > 100) {
-      next(new APIError(400, 'Invalid query parameter `limit` provided.'));
-      return;
-    }
-
     try {
-      const songList = await DB.getSongs(skip, limit);
+      const { skip, limit, scope } = parseQueryParams(req.query);
 
+      const songList = await Songs.getSongs(skip, limit);
       if (!songList.length) {
         throw new APIError(404, 'No songs found.');
       }
 
-      const songs: Array<SongData> = [];
-
-      songList.forEach(({
-        uuid, author, title, cover, likes, uploadedBy,
-      }) => {
-        const song: SongData = {
-          uuid, author, title, cover,
-        };
+      const songs = new Array<SongData>();
+      songList.forEach(({ likes, uploadedBy, ...songData }) => {
+        const song: SongData = { ...songData };
 
         if (scope & scopes.favorite) {
           song.favorite = likes.includes(req.jwt.uuid);
@@ -71,27 +54,26 @@ export function getSongs() {
 export function getSongByUuid() {
   return async (req: AuthorizedRequest, res: Response, next: NextFunction) => {
     try {
-      const song = await DB.getSongByUuid(req.params.songId);
+      const song = await Songs.getByUuid(req.params.songId);
       if (!song) {
         throw new APIError(404, 'No song found.');
       }
 
-      const user = await getUserByUuid(song.uploadedBy);
-
       const {
-        uuid, author, title, cover, path, uploadedBy, likes, createdAt,
+        path,
+        uploadedBy,
+        likes,
+        ...songData
       } = song;
+
+      const user = await Users.getByUuid(uploadedBy);
 
       res.status(200).send({
         message: 'Successfully retrieved song.',
         song: {
-          uuid,
-          author,
-          title,
-          cover,
-          url: `${CDN_SERVER}${path}`,
+          ...songData,
+          url: `${CDN_SERVER}/${path}`,
           uploadedBy: user ? user.username : 'deactivated user',
-          createdAt,
           favorite: likes.includes(req.jwt.uuid),
           edit: uploadedBy === req.jwt.uuid,
         },
@@ -104,42 +86,19 @@ export function getSongByUuid() {
 
 export function findSongs() {
   return async (req: AuthorizedRequest, res: Response, next: NextFunction) => {
-    const { query, scope } = req.query;
-
-    if (!query) {
-      next(new APIError(400, 'No query provided.'));
-      return;
-    }
-
-    const skip = Number(req.query.skip);
-    if (skip < 0) {
-      next(new APIError(400, 'Invalid query parameter `skip` provided.'));
-      return;
-    }
-
-    const limit = Number(req.query.limit);
-    if (limit < 1 || limit > 100) {
-      next(new APIError(400, 'Invalid query parameter `limit` provided.'));
-      return;
-    }
-
     try {
-      const songList = await DB.findSongs(decodeURI(query), skip, limit);
+      const {
+        skip, limit, scope, query,
+      } = parseQueryParams(req.query);
 
+      const songList = await Songs.findSongs(decodeURI(query), skip, limit);
       if (!songList.length) {
         throw new APIError(404, 'No songs found.');
       }
 
       const songs: Array<SongData> = [];
-
-      songList.forEach((songItem) => {
-        const {
-          uuid, author, title, cover, likes, uploadedBy,
-        } = songItem;
-
-        const song: SongData = {
-          uuid, author, title, cover,
-        };
+      songList.forEach(({ likes, uploadedBy, ...songData }) => {
+        const song: SongData = { ...songData };
 
         if (scope & scopes.favorite) {
           song.favorite = likes.includes(req.jwt.uuid);
@@ -182,32 +141,29 @@ export function updateSong() {
       },
     } = req;
 
-    if (!body) {
-      next(new APIError(400, 'No body provided.'));
-      return;
-    }
-
-    const song = await DB.getSongByUuid(songId);
-    if (!song) {
-      next(new APIError(404, 'No song found.'));
-      return;
-    }
-
-    if (song.uploadedBy !== uuid && role < roles.moderator) {
-      next(new APIError(403, 'Forbitten.'));
-      return;
-    }
-
-    const songData = Object.entries(body).reduce((acc, [key, val]) => {
-      if (['author', 'title', 'cover'].includes(key)) {
-        acc[key] = String(val);
+    try {
+      if (!body) {
+        throw new APIError(400, 'No body provided.');
       }
 
-      return acc;
-    }, {} as { [key: string]: string });
+      const song = await Songs.getByUuid(songId);
+      if (!song) {
+        throw new APIError(404, 'No song found.');
+      }
 
-    try {
-      await DB.updateSong(songId, songData);
+      if (song.uploadedBy !== uuid && role < roles.moderator) {
+        throw new APIError(403, 'Forbitten.');
+      }
+
+      const songData = Object.entries(body).reduce((acc, [key, val]) => {
+        if (['author', 'title', 'cover'].includes(key)) {
+          acc[key] = String(val);
+        }
+
+        return acc;
+      }, {} as { [key: string]: string });
+
+      await Songs.updateSong(songId, songData);
 
       res.status(200).send({ message: 'Successfully updated song.', song: songData });
     } catch (e) {
@@ -228,7 +184,7 @@ export function deleteSong() {
     } = req;
 
     try {
-      const song = await DB.getSongByUuid(songId);
+      const song = await Songs.getByUuid(songId);
       if (!song) {
         throw new APIError(404, 'No song found.');
       }
@@ -237,7 +193,7 @@ export function deleteSong() {
         throw new APIError(403, 'Forbitten.');
       }
 
-      await DB.deleteSong(songId);
+      await Songs.deleteSong(songId);
 
       res.status(204).send();
     } catch (e) {
