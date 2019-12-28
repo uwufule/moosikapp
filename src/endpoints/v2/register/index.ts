587b550f0c58fcd5533ce22cb1903e30d8dbb653
Ferisef <ferisefbns@gmail.com>
2019-12-29 00:21:16 +0300

    bcrypt -> bcryptjs. Fix errors. Remove password max length.

--- src/endpoints/v2/register/index.ts
+++ src/endpoints/v2/register/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { MongoError } from 'mongodb';
-import Bcrypt from 'bcrypt';
+import Bcrypt from 'bcryptjs';
 import Joi from '@hapi/joi';
 import { createUser } from '../../../api/mongodb/users';
 import APIError from '../../../errors/APIError';
@@ -16,34 +16,33 @@ import {
 const USERNAME_REGEX = /^([a-z0-9]|[\u30A0-\u30FF]|[\u3040-\u309F])+$/i;
 const PASSWORD_REGEX = /^[\w$.!?\-=~#@]+$/i;
 
+const validationSchema = Joi.object({
+  username: Joi.string()
+    .required()
+    .min(2)
+    .max(24)
+    .regex(USERNAME_REGEX)
+    .error(new Error(INVALID_USERNAME)),
+  email: Joi.string()
+    .required()
+    .email({ allowUnicode: false })
+    .error(new Error(INVALID_EMAIL)),
+  password: Joi.string()
+    .required()
+    .min(8)
+    .regex(PASSWORD_REGEX)
+    .error(new Error(INVALID_PASSWORD)),
+});
+
 export default () => async (req: Request, res: Response) => {
   try {
     if (!req.body) {
       throw new APIError(400, 'No body provided.');
     }
 
-    const validationSchema = Joi.object({
-      username: Joi.string()
-        .required()
-        .min(2)
-        .max(24)
-        .regex(USERNAME_REGEX)
-        .error(new APIError(400, INVALID_USERNAME)),
-      email: Joi.string()
-        .required()
-        .email({ allowUnicode: false })
-        .error(new APIError(400, INVALID_EMAIL)),
-      password: Joi.string()
-        .required()
-        .min(8)
-        .max(64)
-        .regex(PASSWORD_REGEX)
-        .error(new APIError(400, INVALID_PASSWORD)),
-    });
-
     const { error, value } = validationSchema.validate(req.body);
     if (error) {
-      throw new APIError(400, `${error.message.replace(/"/g, '`')}.`);
+      throw new APIError(400, error.message);
     }
 
     const salt = await Bcrypt.genSalt();
