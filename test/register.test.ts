321c8354be6cbb5abc803bca1f864c036fa4e07b
Ferisef <ferisefbns@gmail.com>
2019-12-29 02:46:54 +0300

    Update

--- test/register.test.ts
+++ test/register.test.ts
@@ -1,10 +1,11 @@
 import request from 'supertest';
+import expect from 'expect';
 
 import UserModel from '../src/api/mongodb/models/user.model';
 import app from '../src/index';
 
-beforeEach(async () => {
-  await UserModel.deleteMany({});
+before(async () => {
+  await UserModel.deleteOne({ username: 'testuser1' });
 });
 
 describe('registration', () => {
@@ -12,100 +13,107 @@ describe('registration', () => {
     request(app)
       .post('/api/v2/register')
       .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .send(JSON.stringify({
+      .send({
         username: 'testuser1',
-        email: 'testuser1@domail.com',
+        email: 'testuser1@domain.com',
         password: 'supersecretpassword',
-      }))
+      })
       .expect(201)
       .expect('Content-Type', /application\/json/)
-      .expect(/You have successfully created a new account./, done);
+      .end((req, res) => {
+        expect(res.body.message).toBe('You have successfully created a new account.');
+        expect(res.body).toHaveProperty('uuid');
+
+        done();
+      });
   });
 
   it('should return Status-Code 405 and correct body if incorrect header `Accept` provided', (done) => {
     request(app)
       .post('/api/v2/register')
-      .set('Content-Type', 'application/json')
-      .send(JSON.stringify({
+      .send({
         username: 'testuser1',
-        email: 'testuser1@domail.com',
+        email: 'testuser1@domain.com',
         password: 'supersecretpassword',
-      }))
+      })
       .expect(405)
       .expect('Content-Type', /application\/json/)
-      .expect(/Incorrect `Accept` header provided./, done);
+      .expect({ message: 'Incorrect `Accept` header provided.' }, done);
   });
 
   it('should return Status-Code 400 and correct body if incorrect header `Content-Type` provided', (done) => {
     request(app)
       .post('/api/v2/register')
       .set('Accept', 'application/json')
-      .send(JSON.stringify({
-        username: 'testuser1',
-        email: 'testuser1@domail.com',
-        password: 'supersecretpassword',
-      }))
+      .send('')
       .expect(400)
       .expect('Content-Type', /application\/json/)
-      .expect(/Invalid body provided./, done);
+      .expect({ message: 'Invalid body provided.' }, done);
   });
 
   it('should return Status-Code 400 and correct body if user entered invalid username', (done) => {
     request(app)
       .post('/api/v2/register')
       .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .send(JSON.stringify({
+      .send({
         username: 'incorrect username',
         email: 'testuser1@domain.com',
         password: 'supersecretpassword',
-      }))
+      })
       .expect(400)
       .expect('Content-Type', /application\/json/)
-      .expect(/Invalid username provided./, done);
+      .end((req, res) => {
+        expect(res.body.message).toMatch(/Invalid username provided./);
+
+        done();
+      });
   });
 
   it('should return Status-Code 400 and correct body if user entered invalid email', (done) => {
     request(app)
       .post('/api/v2/register')
       .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .send(JSON.stringify({
+      .send({
         username: 'testuser1',
         email: 'invalid email',
         password: 'supersecretpassword',
-      }))
+      })
       .expect(400)
       .expect('Content-Type', /application\/json/)
-      .expect(/Invalid e-mail address provided./, done);
+      .end((req, res) => {
+        expect(res.body.message).toMatch(/Invalid e-mail address provided./);
+
+        done();
+      });
   });
 
   it('should return Status-Code 400 and correct body if user entered invalid password', (done) => {
     request(app)
       .post('/api/v2/register')
       .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .send(JSON.stringify({
+      .send({
         username: 'username',
         email: 'email@moosikapp.tk',
         password: 'short',
-      }))
+      })
       .expect(400)
       .expect('Content-Type', /application\/json/)
-      .expect(/Invalid password provided./, done);
+      .end((req, res) => {
+        expect(res.body.message).toMatch(/Invalid password provided./);
+
+        done();
+      });
   });
 
   it('should return Status-Code 400 and correct body if user with provided username/email already exists', (done) => {
     request(app)
       .post('/api/v2/register')
       .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .send(JSON.stringify({
+      .send({
         username: 'testuser1',
-        email: 'testuser1@domail.com',
+        email: 'testuser1@domain.com',
         password: 'supersecretpassword',
-      }))
+      })
       .end(() => {
         request(app)
           .post('/api/v2/register')
@@ -113,12 +121,12 @@ describe('registration', () => {
           .set('Content-Type', 'application/json')
           .send(JSON.stringify({
             username: 'testuser1',
-            email: 'testuser1@domail.com',
+            email: 'testuser1@domain.com',
             password: 'supersecretpassword',
           }))
           .expect(400)
           .expect('Content-Type', /application\/json/)
-          .expect(/An account with that email address and\/or username already exists./, done);
+          .expect({ message: 'An account with that email address and/or username already exists.' }, done);
       });
   });
 });
