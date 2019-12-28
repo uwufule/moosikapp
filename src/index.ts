999fcaff4e321f48325137b4a80562b66e9c0881
Ferisef <ferisefbns@gmail.com>
2019-12-28 22:32:45 +0300

    Add default export

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,23 @@ import { MAX_FILE_SIZE } from './config/constants.json';
 
 const { PORT } = process.env;
 
-async function main() {
-  await mongoDB();
+const app = Express();
 
-  const app = Express();
+mongoDB()
+  .then(() => {
+    app.use(helmet({ hsts: false }));
+    app.use(cors());
 
-  app.use(helmet({ hsts: false }));
-  app.use(cors());
+    app.use(Express.static(Path.resolve('static')));
 
-  app.use(Express.static(Path.resolve('static')));
+    app.use(BodyParser.json());
+    app.use(BodyParser.raw({ type: 'audio/mpeg', limit: MAX_FILE_SIZE }));
 
-  app.use(BodyParser.json());
-  app.use(BodyParser.raw({ type: 'audio/mpeg', limit: MAX_FILE_SIZE }));
+    withApiEndpoints(app);
 
-  withApiEndpoints(app);
+    app.use(errorHandler());
 
-  app.use(errorHandler());
+    app.listen(Number(PORT));
+  });
 
-  app.listen(Number(PORT));
-}
-
-main();
+export default app;
