b7ac5e4f62fc0f987488c8a92cf07d335c426062
Ferisef <ferisefbns@gmail.com>
2020-01-04 16:34:13 +0300

    Update upload. Remove verify

--- src/endpoints/v2/songs/upload.ts
+++ src/endpoints/v2/songs/upload.ts
@@ -1,71 +1,49 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response } from 'express';
 import { Readable } from 'stream';
-import UUID from 'uuid';
-import request from 'request';
+import Crypto from 'crypto';
+import JWT from 'jsonwebtoken';
+import request, { CoreOptions } from 'request';
 import * as Songs from '../../../api/mongodb/songs';
 import { AuthorizedRequest } from '../../../middlewares/authorization';
-import APIError from '../../../errors/APIError';
 
-const { CDN_SERVER = '' } = process.env;
+import messages from './messages.json';
 
-const uploadTargetList = new Map<string, NodeJS.Timeout>();
+const { CDN_SERVER = '', JWT_SECRET } = process.env;
 
-export default (req: AuthorizedRequest, res: Response, next: NextFunction) => {
-  const uploadTarget = UUID.v4();
-
-  uploadTargetList.set(uploadTarget, setTimeout(() => {
-    uploadTargetList.delete(uploadTarget);
-  }, 600000));
-
+export default async (req: AuthorizedRequest, res: Response) => {
   const readable = new Readable();
   readable._read = () => {}; // eslint-disable-line no-underscore-dangle
 
   readable.push(req.body);
   readable.push(null);
 
-  const targetUri = `${CDN_SERVER}/upload-target/${uploadTarget}`;
+  const hex = Crypto.randomBytes(6).toString('hex');
+  const target = JWT.sign({ hex }, String(JWT_SECRET), { expiresIn: 180 });
+  const targetUri = `${CDN_SERVER}/upload-target/${target}`;
 
-  readable.pipe(request.put(targetUri, {
+  const requestOptions: CoreOptions = {
     headers: {
       'content-type': req.headers['content-type'],
     },
-  }, async (error, response, body) => {
-    if (error) {
-      next(new APIError(500, 'Internal server error.'));
-      return;
-    }
-
-    switch (response.statusCode) {
-      case 201: {
-        const uuid = await Songs.saveSong({ uploadedBy: req.jwt.uuid, path: body });
-        res.status(201).send({
-          message: 'You have successfully uploaded a new song.',
-          uuid,
-        });
-
-        break;
-      }
-      default:
-        next(new APIError(400, body || 'Error while uploading.'));
-    }
-  }));
-};
-
-export function verify() {
-  return (req: Request, res: Response) => {
-    const { uuid } = req.query;
-
-    if (!uploadTargetList.has(uuid)) {
-      res.status(400).send();
-      return;
-    }
-
-    const uploadTarget = uploadTargetList.get(uuid);
-    if (uploadTarget) {
-      clearTimeout(uploadTarget);
-    }
-    uploadTargetList.delete(uuid);
-
-    res.status(200).send();
   };
-}
+
+  readable.pipe(
+    request.put(targetUri, requestOptions, async (uploadError, uploadResult, uploadMsg) => {
+      if (uploadError) {
+        res.status(500).send({ message: 'Internal server error.' });
+        return;
+      }
+
+      switch (uploadResult.statusCode) {
+        case 201: {
+          const uuid = await Songs.saveSong({ uploadedBy: req.jwt.uuid, path: uploadMsg });
+
+          res.status(201).send({ message: messages.UPLOAD_SUCCESSFULLY, uuid });
+          break;
+        }
+        default:
+          res.status(400).send({ message: uploadMsg || messages.UPLOAD_ERROR });
+      }
+    }),
+  );
+};
