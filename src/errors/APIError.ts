0207eda770ba6782820caab1840765e29cfc01e0
Ferisef <ferisefbns@gmail.com>
2020-01-06 14:54:39 +0300

    Update

--- src/errors/APIError.ts
+++ src/errors/APIError.ts
@@ -1,18 +1,9 @@
-interface IAPIError extends Error {
-  statusCode: number;
-  message: string;
-}
-
-export default class APIError implements IAPIError {
-  readonly name: string;
-
+export default class APIError extends Error {
   readonly statusCode: number;
 
-  readonly message: string;
-
   constructor(statusCode: number, message: string) {
-    this.name = 'APIError';
+    super(message);
     this.statusCode = statusCode;
-    this.message = message;
+    this.name = 'APIError';
   }
 }
