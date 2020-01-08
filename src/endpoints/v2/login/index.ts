c94eda2e6cb2077a38f30c14e5fccd752da65f75
Ferisef <ferisefbns@gmail.com>
2020-01-08 14:17:22 +0300

    Update messages import. Add refreshToken

--- src/endpoints/v2/login/index.ts
+++ src/endpoints/v2/login/index.ts
@@ -1,36 +1,50 @@
 import { Request, Response } from 'express';
+import Crypto from 'crypto';
 import Bcrypt from 'bcryptjs';
 import Joi from '@hapi/joi';
 import JWT from 'jsonwebtoken';
 import { findByUsernameOrEmail, PrivateUserInfo } from '../../../api/mongodb/users';
+import { insert } from '../../../api/mongodb/tokens';
 import APIError from '../../../errors/APIError';
 
-import {
-  INVALID_AUTHORIZATION,
-  INVALID_USERNAME,
-  INVALID_PASSWORD,
-  ACCOUNT_DOESNT_EXISTS,
-  SUCCESS,
-} from './messages.json';
+import messages from './messages.json';
+
+interface TokenChain {
+  token: string;
+  refreshToken: string;
+}
 
 const { JWT_SECRET } = process.env;
 
 const validationSchema = Joi.object({
   username: Joi.string()
     .required()
-    .error(new Error(INVALID_USERNAME)),
+    .error(new Error(messages.login.INVALID_USERNAME)),
   password: Joi.string()
     .required()
-    .error(new Error(INVALID_PASSWORD)),
+    .error(new Error(messages.login.INVALID_PASSWORD)),
 });
 
-async function login(user: PrivateUserInfo, password: string): Promise<string> {
+async function login(user: PrivateUserInfo, password: string): Promise<TokenChain> {
   const comparsionResult = await Bcrypt.compare(password, user.password);
   if (!comparsionResult) {
-    throw new APIError(401, INVALID_AUTHORIZATION);
+    throw new APIError(401, messages.login.INVALID_AUTHORIZATION);
   }
 
-  return JWT.sign({ uuid: user.uuid, role: user.role }, String(JWT_SECRET), { expiresIn: '1d' });
+  const token = JWT.sign({
+    uuid: user.uuid,
+    role: user.role,
+  }, String(JWT_SECRET), { expiresIn: '1d' });
+
+  const record = {
+    userId: user.uuid,
+    payload: Crypto.randomBytes(6).toString('hex'),
+  };
+  const refreshToken = JWT.sign(record, String(JWT_SECRET), { expiresIn: '30d' });
+
+  await insert(record);
+
+  return { token, refreshToken };
 }
 
 export default () => async (req: Request, res: Response) => {
@@ -42,11 +56,12 @@ export default () => async (req: Request, res: Response) => {
 
     const user = await findByUsernameOrEmail(value.username);
     if (!user) {
-      throw new APIError(403, ACCOUNT_DOESNT_EXISTS);
+      throw new APIError(403, messages.login.ACCOUNT_DOESNT_EXISTS);
     }
 
-    const token = await login(user, value.password);
-    res.status(200).send({ message: SUCCESS, token });
+    const tokenChain = await login(user, value.password);
+
+    res.status(200).send({ message: messages.login.SUCCESS, ...tokenChain });
   } catch (e) {
     if (e instanceof APIError) {
       res.status(e.statusCode).send({ message: e.message });
