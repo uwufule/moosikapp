050339d34fc7a612fdd45c593ab908b31f82cac8
Ferisef <ferisefbns@gmail.com>
2020-02-23 18:08:02 +0300

    Minor update

--- src/api/v2/status/index.ts
+++ src/api/v2/status/index.ts
@@ -1,9 +1,9 @@
 import { Request, Response, RequestHandler } from 'express';
-import reqeust from 'request-promise';
+import request from 'request-promise';
 
 const { CDN_SERVER = '' } = process.env;
 
 export default (): RequestHandler => async (req: Request, res: Response) => {
-  const status = await reqeust(`${CDN_SERVER}/status.json`);
+  const status = await request(`${CDN_SERVER}/status.json`);
   res.status(200).send(status);
 };
