5d5096c113cce43bdd7c4042d031c7f3600f4f8a
Ferisef <ferisefbns@gmail.com>
2020-02-23 18:09:41 +0300

    Minor update. Update error message

--- src/middlewares/permissions.ts
+++ src/middlewares/permissions.ts
@@ -1,11 +1,11 @@
-import { Response, NextFunction, RequestHandler } from 'express';
+import { RequestHandler, Response, NextFunction } from 'express';
 import HttpErrors from 'http-errors';
 import { AuthorizedRequest } from './authorization';
 
 export default (minimunRequiredRole: number): RequestHandler => (
   (req: AuthorizedRequest, res: Response, next: NextFunction) => {
     if (req.jwt.role < minimunRequiredRole) {
-      throw new HttpErrors.Forbidden('Access deny.');
+      throw new HttpErrors.Forbidden('Access denied.');
     }
 
     next();
