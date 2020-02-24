08617845dd48ed4348bf1eba86279a60d1e78148
Ferisef <ferisefbns@gmail.com>
2020-02-24 20:04:23 +0300

    Minor update

--- test/songs.test.ts
+++ test/songs.test.ts
@@ -1,8 +1,11 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import FS from 'fs';
+import { promisify } from 'util';
 
 import app from '../src/app';
+
+const readFile = promisify(FS.readFile);
 
 describe.skip('songs', () => {
   beforeEach(async () => {
@@ -11,190 +14,184 @@ describe.skip('songs', () => {
   afterEach(async () => {
   });
 
-  describe('uploading', () => {
-    it.skip('should return Status-Code 201 and correct body if song successfully uploaded', (done) => {
-      FS.readFile('./test/dataset/song.mp3', (err, song) => {
-        if (err) {
-          done(err);
-          return;
-        }
+  describe('upload', () => {
+    it.skip('should return Status-Code 201 and correct body if song successfully uploaded', async () => {
+      const song = await readFile('test/dataset/song.mp3');
 
-        request(app)
-          .post('/api/v2/songs')
-          .set('Accept', 'application/json')
-          .set('Content-Type', 'audio/mpeg')
-          .auth('TOKEN', { type: 'bearer' })
-          .send(song)
-          .expect(201)
-          .expect('Content-Type', /application\/json/)
-          .end((req, res) => {
-            expect(res.body.message).to.eq('You have successfully uploaded a new song.');
-            expect(res.body.uuid).to.be.a('string');
+      const res = await request(app)
+        .post('/api/v2/songs')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'audio/mpeg')
+        .auth('TOKEN', { type: 'bearer' })
+        .send(song);
 
-            done();
-          });
-      });
+      // .expect(201)
+      // .expect('Content-Type', /application\/json/)
+      // .end((req, res) => {
+      //   expect(res.body.message).to.eq('You have successfully uploaded a new song.');
+      //   expect(res.body.uuid).to.be.a('string');
+      // });
     });
 
-    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', (done) => {
+    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 400 and correct body if incorrect header `Content-Type` provided', (done) => {
+    it.skip('should return Status-Code 400 and correct body if incorrect header `Content-Type` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 403 and correct body if invalid token provided', (done) => {
+    it.skip('should return Status-Code 403 and correct body if invalid token provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 413 and correct body if song too large', (done) => {
+    it.skip('should return Status-Code 413 and correct body if song too large', async () => {
 
     });
 
-    it.skip('should return Status-Code 409 and correct body if song already exists', (done) => {
+    it.skip('should return Status-Code 409 and correct body if song already exists', async () => {
 
     });
   });
 
-  describe('retrieving list', () => {
-    it.skip('should return Status-Code 200 and correct body if song list sucessfully retrieved', (done) => {
+  describe('get list', () => {
+    it.skip('should return Status-Code 200 and correct body if song list sucessfully retrieved', async () => {
 
     });
 
-    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', (done) => {
+    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 403 and correct body if invalid token provided', (done) => {
+    it.skip('should return Status-Code 403 and correct body if invalid token provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 400 and correct body if invalid query parameter `skip` provided', (done) => {
+    it.skip('should return Status-Code 400 and correct body if invalid query parameter `skip` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 400 and correct body if invalid query parameter `limit` provided', (done) => {
+    it.skip('should return Status-Code 400 and correct body if invalid query parameter `limit` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 400 and correct body if invalid query parameter `scope` provided', (done) => {
-
-    });
-  });
-
-  describe('retrieving by uuid', () => {
-    it.skip('should return Status-Code 200 and correct body if song sucessfully retrieved', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 403 and correct body if invalid token provided', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 404 and correct body if no song found', (done) => {
+    it.skip('should return Status-Code 400 and correct body if invalid query parameter `scope` provided', async () => {
 
     });
   });
 
-  describe('find songs', () => {
-    it.skip('should return Status-Code 200 and correct body if song list sucessfully retrieved', (done) => {
+  describe('get by uuid', () => {
+    it.skip('should return Status-Code 200 and correct body if song sucessfully retrieved', async () => {
 
     });
 
-    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', (done) => {
+    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 403 and correct body if invalid token provided', (done) => {
+    it.skip('should return Status-Code 403 and correct body if invalid token provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 404 and correct body if no song found', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 400 and correct body if invalid no query parameter `query` provided', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 400 and correct body if invalid query parameter `skip` provided', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 400 and correct body if invalid query parameter `limit` provided', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 400 and correct body if invalid query parameter `scope` provided', (done) => {
+    it.skip('should return Status-Code 404 and correct body if no song found', async () => {
 
     });
   });
 
-  describe('updating song', () => {
-    it.skip('should return Status-Code 200 and correct body if song sucessfully updated', (done) => {
+  describe('find', () => {
+    it.skip('should return Status-Code 200 and correct body if song list sucessfully retrieved', async () => {
 
     });
 
-    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', (done) => {
+    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 400 and correct body if incorrect header `Content-Type` provided', (done) => {
+    it.skip('should return Status-Code 403 and correct body if invalid token provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 403 and correct body if invalid token provided', (done) => {
+    it.skip('should return Status-Code 404 and correct body if no song found', async () => {
 
     });
 
-    it.skip('should return Status-Code 404 and correct body if no song found', (done) => {
+    it.skip('should return Status-Code 400 and correct body if invalid no query parameter `query` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 403 and correct body if user does not have access to edit song', (done) => {
+    it.skip('should return Status-Code 400 and correct body if invalid query parameter `skip` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 400 and correct body if invalid body parameter `author` provided', (done) => {
+    it.skip('should return Status-Code 400 and correct body if invalid query parameter `limit` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 400 and correct body if invalid body parameter `title` provided', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 400 and correct body if invalid body parameter `cover` provided (from formdata)', (done) => {
-
-    });
-
-    it.skip('should return Status-Code 400 and correct body if invalid body parameter `cover` provided (from json)', (done) => {
+    it.skip('should return Status-Code 400 and correct body if invalid query parameter `scope` provided', async () => {
 
     });
   });
 
-  describe('deleting song', () => {
-    it.skip('should return Status-Code 204 and correct body if song sucessfully deleted', (done) => {
+  describe('update', () => {
+    it.skip('should return Status-Code 200 and correct body if song sucessfully updated', async () => {
 
     });
 
-    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', (done) => {
+    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 403 and correct body if invalid token provided', (done) => {
+    it.skip('should return Status-Code 400 and correct body if incorrect header `Content-Type` provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 404 and correct body if no song found', (done) => {
+    it.skip('should return Status-Code 403 and correct body if invalid token provided', async () => {
 
     });
 
-    it.skip('should return Status-Code 403 and correct body if user does not have access to edit song', (done) => {
+    it.skip('should return Status-Code 404 and correct body if no song found', async () => {
+
+    });
+
+    it.skip('should return Status-Code 403 and correct body if user does not have access to edit song', async () => {
+
+    });
+
+    it.skip('should return Status-Code 400 and correct body if invalid body parameter `author` provided', async () => {
+
+    });
+
+    it.skip('should return Status-Code 400 and correct body if invalid body parameter `title` provided', async () => {
+
+    });
+
+    it.skip('should return Status-Code 400 and correct body if invalid body parameter `cover` provided (from formdata)', async () => {
+
+    });
+
+    it.skip('should return Status-Code 400 and correct body if invalid body parameter `cover` provided (from json)', async () => {
+
+    });
+  });
+
+  describe('delete', async () => {
+    it.skip('should return Status-Code 204 and correct body if song sucessfully deleted', async () => {
+
+    });
+
+    it.skip('should return Status-Code 405 and correct body if incorrect header `Accept` provided', async () => {
+
+    });
+
+    it.skip('should return Status-Code 403 and correct body if invalid token provided', async () => {
+
+    });
+
+    it.skip('should return Status-Code 404 and correct body if no song found', async () => {
+
+    });
+
+    it.skip('should return Status-Code 403 and correct body if user does not have access to edit song', async () => {
 
     });
   });
