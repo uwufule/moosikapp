24b676eb8687714c46b72e77f405ab6ec5e5751a
Ferisef <ferisefbns@gmail.com>
2020-03-19 20:51:48 +0300

    Add ssr styled-components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,56 @@
+import Document, {
+  Head, Main, NextScript, DocumentContext,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const styleSheet = new ServerStyleSheet();
+
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        enhanceApp: (App) => (props) => styleSheet.collectStyles(<App {...props} />),
+      });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {styleSheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      styleSheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="application-name" content="Moosik" />
+          <meta name="keywords" content="moosikapp, moosik, music, anime" />
+          <meta name="description" content="" />
+          <link href="/favicon.ico" rel="icon" />
+          <link rel="apple-touch-icon" sizes="256x256" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#212121" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
