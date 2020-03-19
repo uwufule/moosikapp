d3264a8729dcebdd3673702827684e1977883f94
Ferisef <ferisefbns@gmail.com>
2020-03-19 20:53:37 +0300

    Add theme colors and some other variables

---
+++ src/theme/colors.ts
@@ -0,0 +1,26 @@
+enum Colors {
+  blue = '#2196f3',
+  darkBlue = '#1d87da',
+  indigo = '#303f9f',
+  purple = '#673ab7',
+  pink = '#ff4081',
+  red = '#f6685e',
+  darkRed = '#e5625d',
+  orange = '#ff9800',
+  yellow = '#ffeb3b',
+  green = '#4caf50',
+  darkGreen = '#449d48',
+  cyan = '#0097a7',
+  white = '#ffffff',
+  lightGray = '#bdbdbd',
+  gray = '#9e9e9e',
+  darkGray = '#616161',
+  light = '#f5f5f5',
+  dark = '#212121',
+  success = '#4caf50',
+  info = '#0097a7',
+  warning = '#ffeb3b',
+  danger = '#f44336',
+}
+
+export default Colors;
---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+import colors from './colors';
+
+const theme = {
+  colors,
+  font: 'Arial, Helvetica, sans-serif',
+  transition: '200ms ease',
+  shadow: `0 0 4px ${colors.dark}`,
+};
+
+export default theme;
