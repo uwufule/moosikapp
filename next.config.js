9c4fe1bb519c08198ef070da3abd945edb7a4959
Ferisef <ferisefbns@gmail.com>
2020-03-19 20:46:11 +0300

    Add sass

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
+const withSass = require('@zeit/next-sass');
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-const config = {
+const nextConfig = {
   poweredByHeader: false,
 };
 
 exports.default = withPlugins([
-  withImages,
-], config);
+  [withImages, {}],
+  [withSass, {
+    cssModules: true,
+    cssLoadeOptions: {
+      localIdentName: '[path]__[local]__[hash:base64:5]',
+    },
+    [PHASE_PRODUCTION_BUILD]: {
+      cssLoadeOptions: {
+        localIdentName: 'css-[hash:base64:5]',
+      },
+    },
+  }],
+], nextConfig);
