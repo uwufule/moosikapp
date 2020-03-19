d5d33855a2a90beb6fdac0d86354096728529c4e
Ferisef <ferisefbns@gmail.com>
2020-03-19 20:48:06 +0300

    Init for Layout component

---
+++ src/components/Layout/GlobalStyle.tsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+import { ThemeProps } from '../ThemeProvider';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+    font-family: ${(props: ThemeProps) => props.theme.font};
+  }
+
+  body {
+    height: inherit;
+    margin: 0;
+    background: ${(props: ThemeProps) => props.theme.colors.dark};
+  }
+
+  #__next {
+    height: inherit;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import ThemeProvider from '../ThemeProvider';
+import BackgroundImage from '../BackgroundImage';
+import Header from '../Header';
+import GlobalStyle from './GlobalStyle';
+
+const Main = styled.main`
+  display: flex;
+  min-height: 100%;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  max-width: 1200px;
+  position: relative;
+  margin: 0 auto;
+`;
+
+interface LayoutProps {
+  children: JSX.Element;
+}
+
+export default ({ children }: LayoutProps) => (
+  <ThemeProvider>
+    <GlobalStyle />
+    {/* <Sidebar /> */}
+    <Main>
+      <BackgroundImage />
+      <Content>
+        <Header />
+        {children}
+      </Content>
+      {/* <Player /> */}
+    </Main>
+    {/* <Modal /> */}
+  </ThemeProvider>
+);
