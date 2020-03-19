06495baa32f5ad0a6b5067d4f5a50fc119422831
Ferisef <ferisefbns@gmail.com>
2020-03-19 20:47:52 +0300

    Init for Header component

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import { ThemeProps } from '../ThemeProvider';
+
+const Logo = styled.svg`
+  width: 48px;
+  height: 48px;
+  fill: ${(props: ThemeProps) => props.theme.colors.light};
+  transition: fill ${(props: ThemeProps) => props.theme.transition};
+
+  &:hover {
+    fill: ${(props: ThemeProps) => props.theme.colors.red};
+  }
+`;
+
+interface LogoProps {
+  linkTo: string;
+}
+
+export default ({ linkTo }: LogoProps) => (
+  <Link href={linkTo}>
+    <a aria-label="Logo">
+      <Logo viewBox="0 0 24 24">
+        <path
+          d="M12,1C7,1 3,5 3,10V17A3,3 0 0,0 6,20H9V12H5V10A7,
+            7 0 0,1 12,3A7,7 0 0,1 19,10V12H15V20H18A3,3 0 0,0 21,17V10C21,5 16.97,1 12,1Z"
+        />
+      </Logo>
+    </a>
+  </Link>
+);
