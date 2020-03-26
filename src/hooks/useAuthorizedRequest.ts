d290555ceb2b44471c657a413f798a91a7568fad
Ferisef <ferisefbns@gmail.com>
2020-03-26 15:21:02 +0300

    Use token manager

--- src/hooks/useAuthorizedRequest.ts
+++ src/hooks/useAuthorizedRequest.ts
@@ -1,38 +1,24 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { AxiosRequestConfig } from 'axios';
-import JWT from 'jsonwebtoken';
-import useReleaseTokens from './useReleaseTokens';
+import useTokenManager from './useTokenManager';
 import authorizedRequest from '../utils/transport/authorizedRequest';
-import { setTokenChain } from '../redux/actions/login';
 import { RootState } from '../redux/store';
-
-interface AccessTokenRecord {
-  uuid: string;
-  role: number;
-  iat: number;
-  exp: number;
-}
 
 const useAuthorizedRequest = () => {
   let accessToken = useSelector<RootState, string>(
     (state) => state.login.accessToken,
   );
 
-  const dispatch = useDispatch();
-
-  const releaseTokens = useReleaseTokens();
+  const tokenManager = useTokenManager();
 
   return async (url: string, config?: AxiosRequestConfig) => {
     const refreshToken = localStorage.getItem('refreshToken');
 
-    const record = JWT.decode(accessToken) as AccessTokenRecord;
-    if (record && (record.exp * 1000 - Date.now() < 60000) && refreshToken) {
-      const res = await releaseTokens(refreshToken);
-
-      dispatch(setTokenChain(res.data.token, res.data.refreshToken));
-      localStorage.setItem('refreshToken', res.data.refreshToken);
-
-      accessToken = res.data.token;
+    if (accessToken && refreshToken) {
+      const tokens = await tokenManager.releaseTokens(accessToken, refreshToken);
+      if (tokens) {
+        accessToken = tokens.accessToken;
+      }
     }
 
     return authorizedRequest(url, accessToken, config);
---
+++ src/hooks/useTokenManager.ts
@@ -0,0 +1,57 @@
+import { useDispatch } from 'react-redux';
+import JWT from 'jsonwebtoken';
+import useRequest from './useRequest';
+import { setTokenChain } from '../redux/actions/login';
+import { RootState } from '../redux/store';
+
+interface AccessTokenRecord {
+  uuid: string;
+  role: number;
+  iat: number;
+  exp: number;
+}
+
+interface TokenPair {
+  accessToken: string;
+  refreshToken: string;
+}
+
+const useTokenManeger = () => {
+  const dispatch = useDispatch();
+
+  const request = useRequest();
+
+  const releaseTokens = async (
+    accessToken: string,
+    refreshToken: string,
+  ): Promise<TokenPair | null> => {
+    const record = <AccessTokenRecord>JWT.decode(accessToken);
+    if (record && (record.exp * 1000 - Date.now() < 29 * 60000 + 55000)) {
+      const res = await request(
+        '/login/refresh',
+        {
+          method: 'GET',
+          params: {
+            refreshToken,
+          },
+        },
+      );
+
+      dispatch(setTokenChain(res.data.token, res.data.refreshToken));
+      localStorage.setItem('refreshToken', res.data.refreshToken);
+
+      return {
+        accessToken: res.data.token,
+        refreshToken: res.data.refreshToken,
+      };
+    }
+
+    return null;
+  };
+
+  return {
+    releaseTokens,
+  };
+};
+
+export default useTokenManeger;
