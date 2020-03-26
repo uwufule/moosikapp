d7461d578db110ec162b56e673fd4c8ce4b803db
Ferisef <ferisefbns@gmail.com>
2020-03-26 13:45:30 +0300

    Add searching songs

--- src/pages/music/search.tsx
+++ src/pages/music/search.tsx
@@ -1,14 +1,54 @@
+import { useState, useEffect } from 'react';
 import useWithAuthorization from '../../hooks/useWithAuthorization';
+import useAuthorizedRequest from '../../hooks/useAuthorizedRequest';
 import { Nav, SearchForm, SongList } from '../../components/Music';
 
 const MusicSearch = () => {
+  const [query, setQuery] = useState('');
+  const [searching, setSearching] = useState(false);
+  const [songs, setSongs] = useState([]);
+
   useWithAuthorization();
+
+  const request = useAuthorizedRequest();
+
+  useEffect(() => {
+    const asyncEffect = async () => {
+      setSearching(true);
+
+      try {
+        const res = await request('/songs/search', {
+          method: 'GET',
+          params: {
+            query,
+          },
+        });
+
+        setSongs(res.data.songs);
+      } catch (e) {
+        if (e.response.statusCode !== 404) {
+          // error message
+        }
+
+        setSongs([]);
+      } finally {
+        setSearching(false);
+      }
+    };
+
+    if (query.length < 2) {
+      // error message
+      return;
+    }
+
+    asyncEffect();
+  }, [query]);
 
   return (
     <section>
       <Nav />
-      <SearchForm handler={() => {}} />
-      <SongList songs={[]} searching />
+      <SearchForm handler={setQuery} />
+      <SongList songs={songs} searching={query.length < 2 || searching} />
     </section>
   );
 };
