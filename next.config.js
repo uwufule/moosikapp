525b98df3bce19c3982e396d14cb1230a47ca3f3
Ferisef <ferisefbns@gmail.com>
2020-03-27 18:19:42 +0300

    Remove unused packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
-const withSass = require('@zeit/next-sass');
-const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
 const nextConfig = {
   poweredByHeader: false,
@@ -9,15 +7,4 @@ const nextConfig = {
 
 exports.default = withPlugins([
   [withImages, {}],
-  [withSass, {
-    cssModules: true,
-    cssLoadeOptions: {
-      localIdentName: '[path]__[local]__[hash:base64:5]',
-    },
-    [PHASE_PRODUCTION_BUILD]: {
-      cssLoadeOptions: {
-        localIdentName: 'css-[hash:base64:5]',
-      },
-    },
-  }],
 ], nextConfig);
