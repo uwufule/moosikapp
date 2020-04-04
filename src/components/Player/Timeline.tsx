338d19284cbc6e204f5da2e330c215e99069216d
Ferisef <ferisefbns@gmail.com>
2020-04-04 23:42:27 +0300

    Update timeline. Add timeline handle. Fix class genaration

--- src/components/Player/Timeline.tsx
+++ src/components/Player/Timeline.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import moment from 'moment';
 import { Theme } from '../ThemeProvider';
@@ -22,59 +23,108 @@ const Time = styled.span`
 `;
 
 const PropgressWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 0 6px;
-    height: 14px;
-    flex: 1;
-    outline: 0;
-    cursor: pointer;
+  position: relative;
+  height: 16px;
+  margin: 0 10px;
+  padding: 7px 0;
+  outline: 0;
+  cursor: pointer;
+  user-select: none;
+  flex: 1;
 `;
 
 const ProgressBar = styled.div`
   width: 100%;
   height: 2px;
+  position: absolute;
   background: ${(props: Theme) => props.theme.colors.gray};
 `;
 
-type ProgressActiveProps = Theme<{ percent: number }>;
+type ProgressBarProps = Theme<{ percent: number }>;
 
-const ProgressActive = styled.div<ProgressActiveProps>`
-  width: ${(props: ProgressActiveProps) => (
-    props.percent > 100 ? 100 : props.percent
-  )}%;
-  height: 100%;
-  background: ${(props: ProgressActiveProps) => props.theme.colors.red};
-  transition: background-color ${(props: ProgressActiveProps) => props.theme.transition};
+const ProgressBarActive = styled.div.attrs(
+  (props: ProgressBarProps) => ({ style: { width: `${props.percent}%` } }),
+)<ProgressBarProps>`
+  min-width: 0px;
+  max-width: 100%;
+  height: 2px;
+  position: absolute;
+  background: ${(props: ProgressBarProps) => props.theme.colors.red};
+  transition: background-color ${(props: ProgressBarProps) => props.theme.transition};
+`;
+
+const ProgressBarHandle = styled.div.attrs(
+  (props: ProgressBarProps) => ({ style: { left: `${props.percent}%` } }),
+)<ProgressBarProps>`
+  width: 8px;
+  height: 8px;
+  position: absolute;
+  margin: -3px -3px 0 0 ;
+  border-radius: 100%;
+  background: ${(props: ProgressBarProps) => props.theme.colors.red};
 `;
 
 interface TimelineProps {
   timePassed?: number;
   duration?: number;
-  handler: (event: number) => void;
+  handler: (time: number) => void;
 }
 
-const Timeline = ({ timePassed = 0, duration = 0, handler }: TimelineProps) => (
-  <Wrapper>
-    <Time>{moment(timePassed * 1000).format('mm:ss')}</Time>
-    <PropgressWrapper
-      role="slider"
-      aria-valuemax={duration}
-      aria-valuemin={0}
-      aria-valuenow={timePassed}
-      tabIndex={-1}
-      onKeyDown={null}
-      onClick={(event) => {
-        const el = event.currentTarget.getBoundingClientRect();
-        handler((duration * (event.clientX - el.left)) / el.width);
-      }}
-    >
-      <ProgressBar>
-        <ProgressActive percent={100 * (timePassed / duration)} />
-      </ProgressBar>
-    </PropgressWrapper>
-    <Time>{moment(duration * 1000).format('mm:ss')}</Time>
-  </Wrapper>
-);
+const Timeline = ({ timePassed = 0, duration = 0, handler }: TimelineProps) => {
+  const [currentTime, setCurrentTime] = useState(timePassed);
+  const [isLeftMouseButtonPressed, setIsLeftMouseButtonPressed] = useState(false);
+
+  useEffect(() => {
+    if (!isLeftMouseButtonPressed) {
+      setCurrentTime(timePassed);
+    }
+  }, [timePassed]);
+
+  const percent = 100 * (currentTime / duration);
+
+  return (
+    <Wrapper>
+      <Time>{moment(currentTime * 1000).format('mm:ss')}</Time>
+      <PropgressWrapper
+        role="slider"
+        aria-valuemin={0}
+        aria-valuemax={duration}
+        aria-valuenow={timePassed}
+        onMouseDown={(event) => {
+          if (event.button === 0) {
+            setIsLeftMouseButtonPressed(true);
+          }
+        }}
+        onMouseMove={(event) => {
+          if (isLeftMouseButtonPressed) {
+            const { left, width } = event.currentTarget.getBoundingClientRect();
+            setCurrentTime((duration * (event.clientX - left)) / width);
+          }
+        }}
+        onMouseUp={(event) => {
+          if (event.button === 0) {
+            const { left, width } = event.currentTarget.getBoundingClientRect();
+            const time = (duration * (event.clientX - left)) / width;
+            handler(time);
+            setCurrentTime(time);
+
+            setIsLeftMouseButtonPressed(false);
+          }
+        }}
+        onMouseLeave={() => {
+          if (isLeftMouseButtonPressed) {
+            handler(currentTime);
+            setIsLeftMouseButtonPressed(false);
+          }
+        }}
+      >
+        <ProgressBar />
+        <ProgressBarActive percent={percent} />
+        <ProgressBarHandle percent={percent} />
+      </PropgressWrapper>
+      <Time>{moment(duration * 1000).format('mm:ss')}</Time>
+    </Wrapper>
+  );
+};
 
 export default Timeline;
