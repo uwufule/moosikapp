c4b6ff9ea3f525c280b546fbbefb41bbcf0572c1
Ferisef <ferisefbns@gmail.com>
2020-04-19 16:59:53 +0300

    Minor update

--- src/server/mongodb/index.ts
+++ src/server/mongodb/index.ts
@@ -1,10 +1,10 @@
 import Mongoose from 'mongoose';
 
-const { MONGO_URI = '' } = process.env;
+const { MONGO_URI } = process.env;
 
 export default async () => {
   await Mongoose.connect(
-    MONGO_URI,
+    String(MONGO_URI),
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
