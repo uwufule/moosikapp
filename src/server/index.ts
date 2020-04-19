9a1a12f37195d933489bed2b2e7db71eb630e13f
Ferisef <ferisefbns@gmail.com>
2020-04-19 21:25:12 +0300

    Remove static resolving

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,4 @@
-import Path from 'path';
-import Express from 'express';
+import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import BodyParser from 'body-parser';
@@ -9,14 +8,12 @@ import asyncErrorHandler from './middlewares/asyncErrorHandler';
 
 import { MAX_FILE_SIZE } from './config/constants.json';
 
-const app = Express();
+const app = express();
 
 mongoDB();
 
 app.use(helmet({ hsts: false }));
 app.use(cors());
-
-app.use(Express.static(Path.resolve('static')));
 
 app.use(BodyParser.json());
 app.use(BodyParser.raw({ type: 'audio/mpeg', limit: MAX_FILE_SIZE }));
