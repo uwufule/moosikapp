ec9e57a9011c3dee47069ea3bae45b461373b0e1
Ferisef <ferisefbns@gmail.com>
2020-05-02 21:10:36 +0300

    redux dev

--- src/frontend/redux/store.ts
+++ src/frontend/redux/store.ts
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import login, { LoginState, LoginAction } from './reducers/login';
 import player, { PlayerState } from './player/reducers';
