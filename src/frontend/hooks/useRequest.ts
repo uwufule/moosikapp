63769258f637fccab8615b88fa9fc01c7841413a
Ferisef <ferisefbns@gmail.com>
2020-05-04 21:56:50 +0300

    Minor update

--- src/frontend/hooks/useRequest.ts
+++ src/frontend/hooks/useRequest.ts
@@ -1,10 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import baseRequest from '../utils/transport/baseRequest';
 
-const useRequest = () => (
-  async (url: string, config?: AxiosRequestConfig) => (
-    baseRequest(url, config)
-  )
-);
+const useRequest = () => (url: string, config?: AxiosRequestConfig) => baseRequest(url, config);
 
 export default useRequest;
