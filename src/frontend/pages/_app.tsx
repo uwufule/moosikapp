e60378b344f264ea57011bd2655f083dff9e5aef
Ferisef <ferisefbns@gmail.com>
2020-05-04 22:36:52 +0300

    Remove getInitialProps

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import { Store } from 'redux';
 import { Provider } from 'react-redux';
-import { AppContext } from 'next/app';
 import Head from 'next/head';
 import withRedux from 'next-redux-wrapper';
 import createStore from '../redux/store';
@@ -15,23 +14,15 @@ interface AppProps {
   store: Store;
 }
 
-const App = ({ Component, pageProps, store }: AppProps) => (
+const App = ({ Component, store }: AppProps) => (
   <Provider store={store}>
     <Head>
       <title>Moosik</title>
     </Head>
     <Layout>
-      <Component statusCode={pageProps?.statusCode} />
+      <Component />
     </Layout>
   </Provider>
 );
-
-App.getInitialProps = async ({ Component, ctx }: AppContext) => {
-  const pageProps = typeof Component.getInitialProps === 'function'
-    ? await Component.getInitialProps(ctx)
-    : {};
-
-  return { pageProps };
-};
 
 export default withRedux(createStore)(App);
