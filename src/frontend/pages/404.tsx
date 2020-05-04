c1f8ca94cd159353592a5e8299e32388e6908545
Ferisef <ferisefbns@gmail.com>
2020-05-04 21:46:01 +0300

    Add 404 error page

---
+++ src/frontend/pages/404.tsx
@@ -0,0 +1,10 @@
+import Error from '../components/Error';
+
+const NotFoundError = () => (
+  <Error
+    title="404"
+    message={['An error 404 occurred on server']}
+  />
+);
+
+export default NotFoundError;
