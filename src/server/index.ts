93132fce6cc56110199430549e0e04710ba6d0ed
Ferisef <ferisefbns@gmail.com>
2020-06-04 17:55:33 +0300

    Move body-parser initialization in api routes file

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,9 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
-import BodyParser from 'body-parser';
 import connectToMongoDB from './mongodb';
 import routes from './routes';
 import asyncErrorHandler from './middlewares/asyncErrorHandler';
-
-import { MAX_FILE_SIZE } from './config/constants.json';
 
 const init = async () => {
   const app = express();
@@ -15,9 +12,6 @@ const init = async () => {
 
   app.use(helmet({ hsts: false }));
   app.use(cors());
-
-  app.use(BodyParser.json());
-  app.use(BodyParser.raw({ type: 'audio/mpeg', limit: MAX_FILE_SIZE }));
 
   app.use(routes());
 
--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import BodyParser from 'body-parser';
 import v2 from './v2';
+import { MAX_FILE_SIZE } from '../../config/constants.json';
 
 export default () => {
   const router = Router();
+
+  router.use(BodyParser.json());
+  router.use(BodyParser.raw({ type: 'audio/mpeg', limit: MAX_FILE_SIZE }));
 
   router.use('/v2', v2());
 
