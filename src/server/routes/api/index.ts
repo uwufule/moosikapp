1c1d0245880157aed9fb54c056f665965822b485
Ferisef <ferisefbns@gmail.com>
2020-06-04 17:56:56 +0300

    Add method not allowed for nonexistent api routes

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import BodyParser from 'body-parser';
+import { MethodNotAllowed } from 'http-errors';
 import v2 from './v2';
 import { MAX_FILE_SIZE } from '../../config/constants.json';
 
@@ -10,6 +11,11 @@ export default () => {
   router.use(BodyParser.raw({ type: 'audio/mpeg', limit: MAX_FILE_SIZE }));
 
   router.use('/v2', v2());
+
+  router.all('*', () => {
+    // or 'The resource you are trying to request does not exist.'
+    throw new MethodNotAllowed('Method not allowed.');
+  });
 
   return router;
 };
