f888bac62ea7de55f5da195f29698e305f373a23
Ferisef <ferisefbns@gmail.com>
2020-06-07 20:43:57 +0300

    Update svg storing

--- src/frontend/components/Music/Song.tsx
+++ src/frontend/components/Music/Song.tsx
@@ -24,7 +24,7 @@ const Cover = styled.div<CoverProps>`
   background-size: cover;
 `;
 
-const DefaultCover = styled.svg.attrs({ viewBox: '0 0 24 24' })`
+const DefaultCoverSvg = styled.svg.attrs({ viewBox: '0 0 24 24' })`
   width: 32px;
   height: 32px;
   fill: ${(props: Theme) => props.theme.colors.cover.foreground};
@@ -143,6 +143,43 @@ const Wrapper = styled.div`
   }
 `;
 
+const DefaultCover = () => (
+  <DefaultCoverSvg>
+    <path
+      d="M12,3V12.26C11.5,12.09 11,12 10.5,12C8,12 6,14 6,16.5C6,19 8,21
+        10.5,21C13,21 15,19 15,16.5V6H19V3H12Z"
+    />
+  </DefaultCoverSvg>
+);
+
+const SvgPaths = {
+  Play: () => (
+    <path
+      d="M15,16H13V8H15M11,16H9V8H11M12,2A10,10 0 0,0 2,12A10,10 0 0,0
+        12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
+    />
+  ),
+  Pause: () => (
+    <path
+      d="M10,16.5V7.5L16,12M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0
+        0,0 22,12A10,10 0 0,0 12,2Z"
+    />
+  ),
+  Edit: () => (
+    <path
+      d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9
+        16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+    />
+  ),
+  Favorite: () => (
+    <path
+      d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3
+        10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27
+        18.6,15.36 13.45,20.03L12,21.35Z"
+    />
+  ),
+};
+
 interface SongProps {
   uuid: string;
   author: string;
@@ -156,10 +193,12 @@ const Song = ({
   uuid, author, title, cover, favorite = true, edit,
 }: SongProps) => {
   const [fav, setFav] = useState(favorite);
+
+  const songList = useSelector<RootState, ISong[]>(
     (state) => state.player.songList,
   );
 
-  const currentSong = useSelector<RootState, CurrentSong | null>(
+  const currentSong = useSelector<RootState, SongDetails | null>(
     (state) => state.player.current.song,
   );
 
@@ -190,31 +229,21 @@ const Song = ({
   return (
     <Wrapper>
       <Cover coverUrl={cover}>
-        {!cover && (
-          <DefaultCover>
-            <path
-              d="M12,3V12.26C11.5,12.09 11,12 10.5,12C8,12 6,14 6,16.5C6,19 8,21
-                10.5,21C13,21 15,19 15,16.5V6H19V3H12Z"
-            />
-          </DefaultCover>
-        )}
+        {!cover && <DefaultCover />}
         <PlayPauseButton
-          title="Play / Pause"
+          title={playing ? 'Pause' : 'Play'}
           onClick={() => {
             if (currentSong?.uuid === uuid) {
-              dispatch(togglePlaying(!playing));
+              dispatch(setPlaying(!playing));
               return;
             }
 
-            dispatch(setCurrentSongIndex(songList?.findIndex((s) => s.uuid === uuid)));
+            const songIndex = songList.findIndex((song) => song.uuid === uuid);
+            dispatch(setCurrentSongIndex(songIndex));
           }}
         >
           <svg viewBox="0 0 24 24">
-            <path
-              d={(playing && currentSong?.uuid === uuid)
-                ? 'M15,16H13V8H15M11,16H9V8H11M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z'
-                : 'M10,16.5V7.5L16,12M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z'}
-            />
+            {(playing && currentSong?.uuid === uuid) ? <SvgPaths.Play /> : <SvgPaths.Pause />}
           </svg>
         </PlayPauseButton>
       </Cover>
@@ -226,10 +255,7 @@ const Song = ({
         {edit && (
           <ActionButton title="Edit song">
             <ActionButtonIcon>
-              <path
-                d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9
-                  16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
-              />
+              <SvgPaths.Edit />
             </ActionButtonIcon>
           </ActionButton>
         )}
@@ -239,11 +265,7 @@ const Song = ({
           onClick={toggleFav}
         >
           <ActionButtonIcon>
-            <path
-              d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3
-                10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27
-                18.6,15.36 13.45,20.03L12,21.35Z"
-            />
+            <SvgPaths.Favorite />
           </ActionButtonIcon>
         </ActionButton>
       </Actions>
@@ -251,4 +273,4 @@ const Song = ({
   );
 };
 
-export default SongComponent;
+export default Song;
