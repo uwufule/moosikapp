26d021bc01560db557542fd451a157f55586a5d8
Ferisef <ferisefbns@gmail.com>
2020-06-07 20:37:06 +0300

    Use deauthorize

--- src/frontend/components/Header/Logout.tsx
+++ src/frontend/components/Header/Logout.tsx
@@ -2,23 +2,16 @@ import { MouseEvent } from 'react';
 import useAuth from '@hooks/useAuthorization';
 import { Link } from '@components/BaseNav';
 
-const logout = (accessToken: string) => request('/logout', accessToken, { method: 'POST' });
-
 const Logout = () => {
-  const accessToken = useSelector<RootState, string>((state) => state.auth.accessToken);
-
-  const dispatch = useDispatch();
+  const { deauthorize } = useAuth();
 
   const handleClick = async (event: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>) => {
     event.preventDefault();
 
     try {
-      await logout(accessToken);
+      await deauthorize();
     } catch (e) {
-      // error
-    } finally {
-      localStorage.removeItem('refreshToken');
-      dispatch(removeTokenPair());
+      // error message (e.response.data)
     }
   };
 
