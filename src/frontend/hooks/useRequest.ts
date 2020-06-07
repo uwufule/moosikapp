f88838fa7a60bdaeefa0f88ee7c973ebcb07b97d
Ferisef <ferisefbns@gmail.com>
2020-06-07 20:33:34 +0300

    Move authorized request to useRequest

--- src/frontend/hooks/useRequest.ts
+++ src/frontend/hooks/useRequest.ts
@@ -1,6 +1,42 @@
+import { useSelector } from 'react-redux';
 import { AxiosRequestConfig } from 'axios';
-import baseRequest from '../utils/transport/baseRequest';
+import merge from 'lodash/merge';
+import useTokenManager from '@hooks/useTokenManager';
+import { RootState } from '@redux/store';
+import baseRequest from '@utils/request';
 
-const useRequest = () => (url: string, config?: AxiosRequestConfig) => baseRequest(url, config);
+const useRequest = () => {
+  const accessToken = useSelector<RootState, string>(
+    (state) => state.auth.accessToken,
+  );
+
+  const { refresh, isAccessTokenExpiresSoon } = useTokenManager();
+
+  const request = (url: string, config?: AxiosRequestConfig) => (
+    baseRequest(url, merge(config, {
+      headers: {
+        accept: 'application/json',
+      },
+    }))
+  );
+
+  const authRequest = async (url: string, config?: AxiosRequestConfig) => {
+    if (isAccessTokenExpiresSoon()) {
+      await refresh();
+    }
+
+    return baseRequest(url, merge(config, {
+      headers: {
+        accept: 'application/json',
+        authorization: `Bearer ${accessToken}`,
+      },
+    }));
+  };
+
+  return {
+    request,
+    authRequest,
+  };
+};
 
 export default useRequest;
