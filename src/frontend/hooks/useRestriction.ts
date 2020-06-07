86991368966282b14c3b317fcc58a20d06af25f2
Ferisef <ferisefbns@gmail.com>
2020-06-07 20:49:53 +0300

    Update import

--- src/frontend/hooks/useRestriction.ts
+++ src/frontend/hooks/useRestriction.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
-import { RootState } from '../redux/store';
+import { RootState } from '@redux/store';
 
 const useRestriction = () => {
   const router = useRouter();
