9f82be0d790d50c5bb9f442302cdc9a0e1e7e2fb
Ferisef <ferisefbns@gmail.com>
2020-06-07 20:31:34 +0300

    Minor update

--- src/frontend/pages/_document.tsx
+++ src/frontend/pages/_document.tsx
@@ -10,8 +10,10 @@ export default class AppDocument extends Document {
 
     try {
       ctx.renderPage = () => originalRenderPage({
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        enhanceApp: (App) => (props) => styleSheet.collectStyles(<App {...props} />),
+        enhanceApp: (App) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          (props) => styleSheet.collectStyles(<App {...props} />)
+        ),
       });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -33,18 +35,7 @@ export default class AppDocument extends Document {
   render() {
     return (
       <html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="application-name" content="Moosik" />
-          <meta name="keywords" content="moosikapp, moosik, music, anime" />
-          <meta name="description" content="Cool music app." />
-          <link href="/favicon.ico" rel="icon" />
-          <link rel="apple-touch-icon" sizes="256x256" href="/apple-touch-icon.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#212121" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
