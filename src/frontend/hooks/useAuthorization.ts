c3e63d4ea24b630be7e4287e780b4d815123af14
Ferisef <ferisefbns@gmail.com>
2020-06-07 20:39:05 +0300

    Update import. Update localStorage item

--- src/frontend/hooks/useAuthorization.ts
+++ src/frontend/hooks/useAuthorization.ts
@@ -1,44 +1,35 @@
 import { useDispatch } from 'react-redux';
-import useRequest from './useRequest';
-import useAuthorizedRequest from './useAuthorizedRequest';
-import { setTokenPair, removeTokenPair } from '../redux/auth/actions';
+import useRequest from '@hooks/useRequest';
+import { setTokens, removeTokens } from '@redux/auth/actions';
 
-const useAuthorization = () => {
-  const request = useRequest();
-  const authorizedRequest = useAuthorizedRequest();
+const useAuth = () => {
+  const { request, authRequest } = useRequest();
 
   const dispatch = useDispatch();
 
   const authorize = async (username: string, password: string) => {
-    const res = await request(
-      '/login',
-      {
-        method: 'POST',
-        data: {
-          username, password,
-        },
+    const res = await request('/login', {
+      method: 'POST',
+      data: {
+        username, password,
       },
-    );
+    });
 
-    dispatch(setTokenPair(res.data.accessToken, res.data.refreshToken));
-    localStorage.setItem('refreshToken', res.data.refreshToken);
+    dispatch(setTokens(res.data.accessToken, res.data.refreshToken));
+    localStorage.setItem('token', res.data.refreshToken);
   };
 
   const deauthorize = async () => {
-    await authorizedRequest(
-      '/logout',
-      {
-        method: 'POST',
-      },
-    );
+    await authRequest('/logout', { method: 'POST' });
 
-    dispatch(removeTokenPair());
-    localStorage.removeItem('refreshToken');
+    dispatch(removeTokens());
+    localStorage.removeItem('token');
   };
 
   return {
-    authorize, deauthorize,
+    authorize,
+    deauthorize,
   };
 };
 
-export default useAuthorization;
+export default useAuth;
