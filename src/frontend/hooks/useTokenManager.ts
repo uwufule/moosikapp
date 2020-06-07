fcc077f1aeff79f41a4a614b4e641d235bb14ecc
Ferisef <ferisefbns@gmail.com>
2020-06-07 20:34:17 +0300

    Update function names

--- src/frontend/hooks/useTokenManager.ts
+++ src/frontend/hooks/useTokenManager.ts
@@ -1,58 +1,52 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import JWT from 'jsonwebtoken';
-import useRequest from './useRequest';
-import { setTokenPair } from '../redux/auth/actions';
+import { setTokens } from '@redux/auth/actions';
+import { RootState } from '@redux/store';
+import request from '@utils/request';
 
-interface AccessTokenRecord {
+interface AccessToken {
   uuid: string;
   role: number;
   iat: number;
   exp: number;
 }
 
-interface TokenPair {
-  accessToken: string;
-  refreshToken: string;
-}
-
-const useTokenManeger = () => {
+const useTokenManager = () => {
   const dispatch = useDispatch();
 
-  const request = useRequest();
+  const [accessToken, refreshToken] = useSelector<RootState, [string, string]>(
+    (state) => ([
+      state.auth.accessToken,
+      state.auth.refreshToken,
+    ]),
+  );
 
-  const isAccessTokenExpired = (accessToken: string): boolean => {
-    const record = <AccessTokenRecord>JWT.decode(accessToken);
-    if (!record) {
-      throw new Error('Unable to decode access token.');
+  const isAccessTokenExpiresSoon = () => {
+    const payload = <AccessToken>JWT.decode(accessToken);
+    if (!payload) {
+      throw new Error('Invalid access token.');
     }
 
-    return record.exp * 1000 - Date.now() < 60000;
+    const timeLeft = payload.exp - Math.round(Date.now() / 1000);
+    return timeLeft < 60;
   };
 
-  const refreshTokens = async (refreshToken: string): Promise<TokenPair> => {
-    const res = await request(
-      '/login/refresh',
-      {
-        method: 'POST',
-        params: {
-          refreshToken,
-        },
+  const refresh = async (initRefreshToken?: string) => {
+    const res = await request('/login/refresh', {
+      method: 'POST',
+      params: {
+        refreshToken: initRefreshToken || refreshToken,
       },
-    );
+    });
 
-    dispatch(setTokenPair(res.data.accessToken, res.data.refreshToken));
-    localStorage.setItem('refreshToken', res.data.refreshToken);
-
-    return {
-      accessToken: res.data.accessToken,
-      refreshToken: res.data.refreshToken,
-    };
+    dispatch(setTokens(res.data.accessToken, res.data.refreshToken));
+    localStorage.setItem('token', res.data.refreshToken);
   };
 
   return {
-    isAccessTokenExpired,
-    refreshTokens,
+    isAccessTokenExpiresSoon,
+    refresh,
   };
 };
 
-export default useTokenManeger;
+export default useTokenManager;
