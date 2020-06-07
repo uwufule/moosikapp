1db250ce47dc6abc36abb47fda1988e99a513017
Ferisef <ferisefbns@gmail.com>
2020-06-07 20:42:50 +0300

    Update theme choosing

--- src/frontend/components/ThemeProvider.tsx
+++ src/frontend/components/ThemeProvider.tsx
@@ -1,16 +1,25 @@
 import { ThemeProvider, ThemedStyledProps } from 'styled-components';
-import defaultTheme from '../theme/default';
-import darkTheme from '../theme/dark';
+import defaultTheme from '@theme/default';
+import darkTheme from '@theme/dark';
 
 export type Theme<P = {}> = ThemedStyledProps<P, typeof defaultTheme>;
 
+const getTheme = (theme: string) => {
+  switch (theme) {
+    case 'dark':
+      return darkTheme;
+    default:
+      return defaultTheme;
+  }
+};
+
 interface AppThemeProviderProps {
   children: JSX.Element[];
-  darkMode?: boolean;
+  theme?: string; // 'dark' | 'light'
 }
 
-const AppThemeProvider = ({ children, darkMode = false }: AppThemeProviderProps) => (
-  <ThemeProvider theme={darkMode ? darkTheme : defaultTheme}>{children}</ThemeProvider>
+const AppThemeProvider = ({ children, theme = 'default' }: AppThemeProviderProps) => (
+  <ThemeProvider theme={getTheme(theme)}>{children}</ThemeProvider>
 );
 
 export default AppThemeProvider;
