6d34e42f8c34f3cde563551c044810242367c343
Ferisef <ferisefbns@gmail.com>
2020-06-12 14:51:04 +0300

    Refactoring. Update isRefreshTokenExists

--- src/server/mongodb/refreshTokens.ts
+++ src/server/mongodb/refreshTokens.ts
@@ -1,21 +1,18 @@
 import RefreshTokenModel from './models/refreshToken.model';
 
-export const createRefreshToken = async (userId: string): Promise<string> => {
+export const createRefreshToken = async (userId: string) => {
   const { _id: uuid } = await (new RefreshTokenModel({ userId })).save();
   return uuid;
 };
 
-export const deleteRefreshToken = async (uuid: string): Promise<boolean> => {
-  await RefreshTokenModel.findByIdAndDelete(uuid);
-  return true;
+export const deleteRefreshToken = async (uuid: string) => {
+  const res = await RefreshTokenModel.deleteOne({ _id: uuid });
+  return !!res.n;
 };
 
-export const isRefreshTokenExists = async (uuid: string): Promise<boolean> => {
-  const res = await RefreshTokenModel.findById(uuid);
-  return res !== null;
-};
+export const isRefreshTokenExists = (uuid: string) => RefreshTokenModel.exists({ _id: uuid });
 
-export const revokeRefreshTokens = async (userId: string): Promise<boolean> => {
-  const { n = 0 } = await RefreshTokenModel.deleteMany({ userId });
-  return n > 0;
+export const revokeRefreshTokens = async (userId: string) => {
+  const res = await RefreshTokenModel.deleteMany({ userId });
+  return !!res.n;
 };
