a0f5f75db9baf09fbe7ef1285376ac2ac71714c3
Ferisef <ferisefbns@gmail.com>
2020-06-12 14:45:20 +0300

    Change AuthorizedRequest to AuthRequest, jwt -> auth

--- src/server/middlewares/authorization.ts
+++ src/server/middlewares/authorization.ts
@@ -1,39 +1,28 @@
 import {
   RequestHandler, Request, Response, NextFunction,
 } from 'express';
-import HttpErrors from 'http-errors';
 import JWT from 'jsonwebtoken';
-import { getUserByUuid } from '../mongodb/users';
+import HttpErrors from 'http-errors';
+import { AccessToken } from '../utils/tokens';
 
 const { JWT_SECRET } = process.env;
 
-interface AccessToken {
-  uuid: string;
-  role: number;
-}
-
-export interface AuthorizedRequest extends Request {
-  jwt: AccessToken;
+export interface AuthRequest extends Request {
+  auth: AccessToken;
 }
 
 export default (): RequestHandler => (
-  async (req: AuthorizedRequest, res: Response, next: NextFunction) => {
+  async (req: AuthRequest, res: Response, next: NextFunction) => {
     const { authorization } = req.headers;
     if (!authorization || !authorization.startsWith('Bearer')) {
-      throw new HttpErrors.Unauthorized('Invalid authorization.');
+      throw new HttpErrors.Unauthorized('Not authorized.');
     }
 
-    const token = authorization.slice(7);
-
+    const accessToken = authorization.slice(7);
     try {
-      req.jwt = <AccessToken>JWT.verify(token, String(JWT_SECRET));
-    } catch (e) {
-      throw new HttpErrors.Forbidden('Not authorized.');
-    }
-
-    const user = await getUserByUuid(req.jwt.uuid);
-    if (!user) {
-      throw new HttpErrors.Unauthorized('Invalid authorization.');
+      req.auth = <AccessToken>JWT.verify(accessToken, String(JWT_SECRET));
+    } catch {
+      throw new HttpErrors.Forbidden('Invalid authorization.');
     }
 
     next();
