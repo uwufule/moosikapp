3d2df0886f9592a1392fe0d5238c07fa1f424d6f
Ferisef <ferisefbns@gmail.com>
2020-06-12 15:04:44 +0300

    Add more status info

--- src/server/routes/api/v2/status/index.ts
+++ src/server/routes/api/v2/status/index.ts
@@ -1,3 +1,4 @@
+import OS from 'os';
 import { Request, Response, RequestHandler } from 'express';
 import request from 'request-promise';
 
@@ -5,5 +6,17 @@ const { CDN_SERVER = '' } = process.env;
 
 export default (): RequestHandler => async (req: Request, res: Response) => {
   const status = await request(`${CDN_SERVER}/status.json`);
-  res.status(200).send(status);
+  res.status(200).send({
+    api: {
+      cpu: {
+        loadavg: OS.loadavg(),
+      },
+      memory: {
+        free: OS.freemem(),
+        total: OS.totalmem(),
+      },
+      serverTime: new Date(),
+    },
+    cdn: JSON.parse(status),
+  });
 };
