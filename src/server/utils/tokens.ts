398bc3a6fe01a16141d5e1c2627e7cfdf175eaea
Ferisef <ferisefbns@gmail.com>
2020-06-12 13:49:21 +0300

    Add createTokens & updateTokens

---
+++ src/server/utils/tokens.ts
@@ -0,0 +1,57 @@
+import JWT from 'jsonwebtoken';
+import { createRefreshToken, deleteRefreshToken } from '../mongodb/refreshTokens';
+import { AuthPayload } from '../mongodb/users';
+
+const jwtSecret = String(process.env.JWT_SECRET);
+
+interface AccessTokenPayload {
+  uuid: string;
+  role: number;
+}
+
+export interface AccessToken extends AccessTokenPayload {
+  iat: number;
+  exp: number;
+}
+
+const signAccessToken = (payload: AccessTokenPayload) => (
+  JWT.sign(payload, jwtSecret, { expiresIn: '30m' })
+);
+
+interface RefreshTokenPayload {
+  jti: string;
+  sub: string;
+}
+
+export interface RefreshToken extends RefreshTokenPayload {
+  iat: number;
+  exp: number;
+}
+
+const signRefreshToken = (payload: RefreshTokenPayload) => (
+  JWT.sign(payload, jwtSecret, { expiresIn: '30d' })
+);
+
+export const createTokens = async (auth: AuthPayload) => {
+  const accessToken = signAccessToken(auth);
+
+  const refreshToken = signRefreshToken({
+    jti: await createRefreshToken(auth.uuid),
+    sub: auth.uuid,
+  });
+
+  return { accessToken, refreshToken };
+};
+
+export const updateTokens = async (auth: AuthPayload, jti: string) => {
+  const accessToken = signAccessToken(auth);
+
+  await deleteRefreshToken(jti);
+
+  const refreshToken = signRefreshToken({
+    jti: await createRefreshToken(auth.uuid),
+    sub: auth.uuid,
+  });
+
+  return { accessToken, refreshToken };
+};
