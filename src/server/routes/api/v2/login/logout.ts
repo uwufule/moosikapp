92d36bd2b704a0c0db4782b7c201de420b2fc9b4
Ferisef <ferisefbns@gmail.com>
2020-06-12 15:01:11 +0300

    Minor update

--- src/server/routes/api/v2/login/logout.ts
+++ src/server/routes/api/v2/login/logout.ts
@@ -1,15 +1,14 @@
 import { Response, RequestHandler } from 'express';
-import HttpErrors from 'http-errors';
-import { AuthorizedRequest } from '../../../../middlewares/authorization';
+import { Gone } from 'http-errors';
+import { AuthRequest } from '../../../../middlewares/authorization';
 import { revokeRefreshTokens } from '../../../../mongodb/refreshTokens';
 
-import messages from './messages.json';
+export default (): RequestHandler => (
+  async (req: AuthRequest, res: Response) => {
+    if (!(await revokeRefreshTokens(req.auth.uuid))) {
+      throw new Gone('Already logged out.');
+    }
 
-export default (): RequestHandler => async (req: AuthorizedRequest, res: Response) => {
-  const isTokensRevoked = await revokeRefreshTokens(req.jwt.uuid);
-  if (!isTokensRevoked) {
-    throw new HttpErrors.Gone(messages.logout.ALREADY_LOGGED_OUT);
+    res.status(200).send({ message: 'Successfully logged out.' });
   }
-
-  res.status(200).send({ message: messages.logout.SUCCESS });
-};
+);
