036867148587f560de1d2fa7dd177be279557fec
Ferisef <ferisefbns@gmail.com>
2020-06-12 15:03:10 +0300

    Change jwt to auth

--- src/server/routes/api/v2/songs/upload.ts
+++ src/server/routes/api/v2/songs/upload.ts
@@ -1,18 +1,19 @@
 import { Response } from 'express';
-import HttpErrors from 'http-errors';
-import { AuthorizedRequest } from '../../../../middlewares/authorization';
+import { BadRequest } from 'http-errors';
+import { AuthRequest } from '../../../../middlewares/authorization';
 import * as Songs from '../../../../mongodb/songs';
 import upload from '../../../../utils/cdn';
 
-import messages from './messages.json';
-
-export default async (req: AuthorizedRequest, res: Response) => {
+export default async (req: AuthRequest, res: Response) => {
   if (!Buffer.isBuffer(req.body)) {
-    throw new HttpErrors.BadRequest(messages.UPLOAD_ERROR);
+    throw new BadRequest('Invalid body provided.');
   }
 
   const path = await upload('audio/mpeg', req.body);
-  const uuid = await Songs.saveSong({ uploadedBy: req.jwt.uuid, path });
+  const uuid = await Songs.saveSong({ uploadedBy: req.auth.uuid, path });
 
-  res.status(201).send({ message: messages.UPLOAD_SUCCESS, uuid });
+  res.status(201).send({
+    message: 'Successfully upload song.',
+    uuid,
+  });
 };
