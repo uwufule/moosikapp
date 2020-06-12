bb375490531835d5c490793de1613ea9ffdacaad
Ferisef <ferisefbns@gmail.com>
2020-06-12 13:50:08 +0300

    Remove bodyparser raw

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -2,18 +2,15 @@ import { Router } from 'express';
 import BodyParser from 'body-parser';
 import { MethodNotAllowed } from 'http-errors';
 import v2 from './v2';
-import { MAX_FILE_SIZE } from '../../config/constants.json';
 
 export default () => {
   const router = Router();
 
   router.use(BodyParser.json());
-  router.use(BodyParser.raw({ type: 'audio/mpeg', limit: MAX_FILE_SIZE }));
 
   router.use('/v2', v2());
 
   router.all('*', () => {
-    // or 'The resource you are trying to request does not exist.'
     throw new MethodNotAllowed('Method not allowed.');
   });
 
