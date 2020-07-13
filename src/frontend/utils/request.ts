1ee395138a8dd4185426cb04dc5650184329781c
Ferisef <ferisefbns@gmail.com>
2020-07-13 17:35:18 +0300

    Add UploadProgressEvent interface

--- src/frontend/utils/request.ts
+++ src/frontend/utils/request.ts
@@ -3,6 +3,11 @@ import _merge from 'lodash/merge';
 
 const API_URL = '/api/v2';
 
+export interface UploadProgressEvent {
+  loaded: number;
+  total: number;
+}
+
 const request = (url: string, config: AxiosRequestConfig) => (
   axios(`${API_URL}${url}`, _merge(config, {
     headers: {
