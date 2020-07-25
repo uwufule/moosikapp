d9261769fbc1613097e45e715b4b9d720dc297d9
Ferisef <ferisefbns@gmail.com>
2020-07-25 13:56:44 +0300

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:json/recommended',
     'plugin:react/recommended',
     'airbnb-typescript',
+    'prettier',
   ],
   globals: {
     Atomics: 'readonly',
@@ -22,38 +23,32 @@ module.exports = {
     },
     ecmaVersion: 2018,
   },
-  plugins: [
-    'import',
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['import', 'prettier', 'react', '@typescript-eslint'],
   rules: {
     'no-bitwise': 'off',
     'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': ['error', {
-      'components': [
-        'Link',
-      ],
-      'specialLink': [
-        'hrefLeft',
-        'hrefRight',
-      ],
-      'aspects': [
-        'invalidHref',
-        'preferButton',
-      ],
-    }],
-    'jsx-a11y/label-has-associated-control': [2, {
-      'labelAttributes': ['label'],
-      'controlComponents': ['Input'],
-      'depth': 3,
-    }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['label'],
+        controlComponents: ['Input'],
+        depth: 3,
+      },
+    ],
   },
   settings: {
     'import/resolver': {
       typescript: {
         directory: 'src/frontend',
-      }
+      },
     },
   },
 };
