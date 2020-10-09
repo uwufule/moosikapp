e6c5db760d51f494234096c160ccd2ced4ae34d4
Ferisef <ferisefbns@gmail.com>
2020-10-09 13:14:18 +0300

    Minor update

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const webpackNodeExternals = require('webpack-node-externals');
-const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -19,30 +17,26 @@ module.exports = {
     __dirname: true,
   },
   module: {
-    rules: [{
-      test: /\.ts$/,
-      use: {
-        loader: 'ts-loader',
-        options: {
-          configFile: 'tsconfig.json',
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.json',
+          },
         },
       },
-    }, ],
+    ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
-  externals: [
-    webpackNodeExternals(),
-  ],
-  plugins: [
-    new CleanWebpackPlugin(),
-  ],
+  externals: [webpackNodeExternals()],
+  plugins: [new CleanWebpackPlugin()],
   optimization: {
     concatenateModules: true,
     minimize: true,
-    minimizer: [
-      new TerserPlugin(),
-    ],
+    minimizer: [new TerserPlugin()],
   },
 };
