880abcaf55b577b56b75e188bd258e7672976cf2
Ferisef <ferisefbns@gmail.com>
2020-11-16 00:03:18 +0300

    Move Error component

--- src/frontend/pages/404.tsx
+++ src/frontend/pages/404.tsx
@@ -1,5 +1,37 @@
-import Error from '@components/Error';
+import { Theme } from '@components/ThemeProvider';
+import React from 'react';
+import styled from 'styled-components';
 
-const NotFoundError = () => <Error title="404" message={['An error 404 occurred on server']} />;
+const Container = styled.div`
+  position: relative;
+  margin-top: 24px;
+  flex: 1;
+`;
 
-export default NotFoundError;
+const Title = styled.h1`
+  margin: 0;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 1.5;
+  color: ${(props: Theme) => props.theme.text.primary};
+  text-shadow: ${(props: Theme) => props.theme.shadow.long};
+`;
+
+const Message = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.5;
+  color: ${(props: Theme) => props.theme.text.secondary};
+  text-shadow: ${(props: Theme) => props.theme.shadow.long};
+`;
+
+const NotFound: React.FC = () => (
+  <Container>
+    <Title>404</Title>
+    <Message>
+      <p>An error 404 occurred on server</p>
+    </Message>
+  </Container>
+);
+
+export default NotFound;
